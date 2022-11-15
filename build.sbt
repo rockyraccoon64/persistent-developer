c7e98083bfe44ad7f551a2d8c6388fa172327e42
Andrey S <norwegianwood409@gmail.com>
2022-11-15 20:16:30 +0500

    Подключение зависимостей Akka Actor и Logback

--- build.sbt
+++ build.sbt
@@ -6,8 +6,10 @@ version := "0.1"
 scalaVersion := "2.13.10"
 
 libraryDependencies ++= Seq(
+  akkaActor,
   akkaPersistence,
   scalactic,
+  logback,
   akkaPersistenceTestkit % Test,
   scalatest % Test
 )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,12 +6,16 @@ object Dependencies {
     val Scalactic = "3.2.14"
     val Scalatest = "3.2.14"
     val Akka = "2.7.0"
+    val Logback = "1.2.11"
   }
 
   val scalactic: ModuleID = "org.scalactic" %% "scalactic" % Versions.Scalactic
   val scalatest: ModuleID = "org.scalatest" %% "scalatest" % Versions.Scalatest
 
+  val akkaActor: ModuleID = "com.typesafe.akka" %% "akka-actor-typed" % Versions.Akka
   val akkaPersistence: ModuleID = "com.typesafe.akka" %% "akka-persistence-typed" % Versions.Akka
   val akkaPersistenceTestkit: ModuleID = "com.typesafe.akka" %% "akka-persistence-testkit" % Versions.Akka
+
+  val logback: ModuleID = "ch.qos.logback" % "logback-classic" % Versions.Logback
 
 }
