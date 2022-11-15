8c67bc8eefc38591a3380f7b990b70034d14bada
Andrey S <norwegianwood409@gmail.com>
2022-11-15 19:04:57 +0500

    Добавление зависимостей Akka Persistence

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ version := "0.1"
 scalaVersion := "2.13.10"
 
 libraryDependencies ++= Seq(
+  akkaPersistence,
   scalactic,
+  akkaPersistenceTestkit % Test,
   scalatest % Test
 )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,13 @@ object Dependencies {
   object Versions {
     val Scalactic = "3.2.14"
     val Scalatest = "3.2.14"
+    val Akka = "2.7.0"
   }
 
   val scalactic: ModuleID = "org.scalactic" %% "scalactic" % Versions.Scalactic
   val scalatest: ModuleID = "org.scalatest" %% "scalatest" % Versions.Scalatest
+
+  val akkaPersistence: ModuleID = "com.typesafe.akka" %% "akka-persistence-typed" % Versions.Akka
+  val akkaPersistenceTestkit: ModuleID = "com.typesafe.akka" %% "akka-persistence-testkit" % Versions.Akka
 
 }
