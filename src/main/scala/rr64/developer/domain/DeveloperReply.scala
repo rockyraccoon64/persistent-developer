f49053587fa7875dbb435106b34240ad03b06927
Andrey S <norwegianwood409@gmail.com>
2022-11-16 19:58:24 +0500

    [Refactor] Комментарии на ответы разработчика

--- src/main/scala/rr64/developer/domain/DeveloperReply.scala
+++ src/main/scala/rr64/developer/domain/DeveloperReply.scala
@@ -2,8 +2,15 @@ package rr64.developer.domain
 
 import java.util.UUID
 
+/**
+ * Ответ разработчика на команду
+ */
 sealed trait DeveloperReply
 
 object DeveloperReply {
+  /**
+   * Задача принята в разработку
+   * @param id Идентификатор задачи
+   * */
   case class TaskStarted(id: UUID) extends DeveloperReply
 }
