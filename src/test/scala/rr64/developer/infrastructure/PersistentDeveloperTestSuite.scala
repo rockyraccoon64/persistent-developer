157a31b0db0efc0bfa407b0ee1b5d66e0b3fe2da
Andrey S <norwegianwood409@gmail.com>
2022-11-16 20:07:12 +0500

    [Refactor] Устранение дублирования при создании тестового ActorRef

--- src/test/scala/rr64/developer/infrastructure/PersistentDeveloperTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/PersistentDeveloperTestSuite.scala
@@ -1,8 +1,8 @@
 package rr64.developer.infrastructure
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.Scheduler
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior, Scheduler}
 import org.scalatest.flatspec.AsyncFlatSpecLike
 import rr64.developer.domain.{DeveloperReply, Task}
 
@@ -15,6 +15,13 @@ class PersistentDeveloperTestSuite
 
   private implicit val ec: ExecutionContext = testKit.system.executionContext
   private implicit val scheduler: Scheduler = testKit.system.scheduler
+
+  private def mockActorRef(
+    receive: DeveloperBehavior.Command => Behavior[DeveloperBehavior.Command]
+  ): ActorRef[DeveloperBehavior.Command] = {
+    val mockBehavior = Behaviors.receiveMessage(receive)
+    testKit.spawn(mockBehavior)
+  }
 
   /** Команда добавления задачи должна перенаправляться персистентному актору */
   "The Add Task command" should "be redirected to the persistent actor" in {
@@ -31,12 +38,11 @@ class PersistentDeveloperTestSuite
   /** Когда разработчик отвечает "Задача начата", должно приходить соответствующее доменное сообщение */
   "When a task is started, there" should "be a corresponding domain message" in {
     val id = UUID.randomUUID()
-    val mockBehavior = Behaviors.receiveMessage[DeveloperBehavior.Command] {
+    val mockActor = mockActorRef {
       case DeveloperBehavior.AddTask(_, replyTo) =>
         replyTo ! DeveloperBehavior.Replies.TaskStarted(id)
         Behaviors.same
     }
-    val mockActor = testKit.spawn(mockBehavior)
     val dev = PersistentDeveloper(mockActor)
 
     val task = Task(15)
@@ -48,12 +54,11 @@ class PersistentDeveloperTestSuite
   /** Когда разработчик отвечает "Задача поставлена в очередь", приходит соответствующее доменное сообщение */
   "When a task is queued, there" should "be a corresponding domain message" in {
     val id = UUID.randomUUID()
-    val mockBehavior = Behaviors.receiveMessage[DeveloperBehavior.Command] {
+    val mockActor = mockActorRef {
       case DeveloperBehavior.AddTask(_, replyTo) =>
         replyTo ! DeveloperBehavior.Replies.TaskQueued(id)
         Behaviors.same
     }
-    val mockActor = testKit.spawn(mockBehavior)
     val dev = PersistentDeveloper(mockActor)
 
     val task = Task(45)
