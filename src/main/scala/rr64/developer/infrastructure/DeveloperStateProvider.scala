672b39d3569817ac70e2ba4fed99027b5eb08fe1
Andrey S <norwegianwood409@gmail.com>
2022-11-16 21:28:00 +0500

    [Refactor] Комментарий к источнику состояния разработчика

--- src/main/scala/rr64/developer/infrastructure/DeveloperStateProvider.scala
+++ src/main/scala/rr64/developer/infrastructure/DeveloperStateProvider.scala
@@ -4,6 +4,10 @@ import rr64.developer.domain.DeveloperState
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Источник состояния разработчика
+ */
 trait DeveloperStateProvider {
+  /** Состояние разработчика */
   def state(implicit ec: ExecutionContext): Future[DeveloperState]
 }
