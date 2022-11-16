92074979e9f7f339cc3706d8ed8b76a03bd15e4e
Andrey S <norwegianwood409@gmail.com>
2022-11-16 19:09:27 +0500

    [Compile Error] Тест: Команды должны перенаправляться персистентному актору

--- src/test/scala/rr64/developer/infrastructure/PersistentDeveloperTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/PersistentDeveloperTestSuite.scala
@@ -1,14 +1,23 @@
 package rr64.developer.infrastructure
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.flatspec.AnyFlatSpecLike
+import rr64.developer.domain.Task
 
 class PersistentDeveloperTestSuite
-  extends AnyFlatSpec
-    with Matchers {
+  extends ScalaTestWithActorTestKit
+    with AnyFlatSpecLike {
 
-  "This test" should "work" in {
-    true shouldEqual true
+  /** Команды должны перенаправляться персистентному актору */
+  "Commands" should "be redirected to the persistent actor" in {
+    val probe = testKit.createTestProbe[DeveloperBehavior.Command]()
+    val dev = PersistentDeveloper(probe.ref)
+
+    val task = Task(10)
+    dev.addTask(task)
+
+    val command = probe.expectMessageType[DeveloperBehavior.AddTask]
+    command.task shouldEqual task
   }
 
 }
