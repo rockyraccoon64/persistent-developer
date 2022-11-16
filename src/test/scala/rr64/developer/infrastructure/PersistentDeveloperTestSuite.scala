dbae88d31b293efb8dbc332e59e07ed79cae07d8
Andrey S <norwegianwood409@gmail.com>
2022-11-16 18:59:35 +0500

    Каркас для тестов PersistentDeveloper

---
+++ src/test/scala/rr64/developer/infrastructure/PersistentDeveloperTestSuite.scala
@@ -0,0 +1,14 @@
+package rr64.developer.infrastructure
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class PersistentDeveloperTestSuite
+  extends AnyFlatSpec
+    with Matchers {
+
+  "This test" should "work" in {
+    true shouldEqual true
+  }
+
+}
