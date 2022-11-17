875d7b1bfe952465fb3978ded9dc378cf249a0f8
Andrey S <norwegianwood409@gmail.com>
2022-11-17 17:45:22 +0500

    [Refactor] Тестовая фикстура

--- src/test/scala/rr64/developer/infrastructure/task/TaskToRepositoryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/task/TaskToRepositoryTestSuite.scala
@@ -25,70 +25,74 @@ class TaskToRepositoryTestSuite
   private val projectionTestKit = ProjectionTestKit(system)
   private implicit val ec: ExecutionContext = system.executionContext
 
-  private val mockRepository = new TaskRepository { // TODO Fixture
-    private var tasks: Map[UUID, TaskInfo] = Map.empty
-    override def save(taskInfo: TaskInfo): Future[_] = {
-      tasks = tasks.updated(taskInfo.id, taskInfo)
-      Future.unit
+  trait Test {
+
+    protected val mockRepository: TaskRepository = new TaskRepository { // TODO Fixture
+      private var tasks: Map[UUID, TaskInfo] = Map.empty
+      override def save(taskInfo: TaskInfo): Future[_] = {
+        tasks = tasks.updated(taskInfo.id, taskInfo)
+        Future.unit
+      }
+      override def findById(id: UUID): Future[Option[TaskInfo]] =
+        Future.successful(tasks.get(id))
+      override def list: Future[Seq[TaskInfo]] =
+        Future.successful(tasks.values.toSeq)
     }
-    override def findById(id: UUID): Future[Option[TaskInfo]] =
-      Future.successful(tasks.get(id))
-    override def list: Future[Seq[TaskInfo]] =
-      Future.successful(tasks.values.toSeq)
-  }
 
-  private val handler: Handler[EventEnvelope[Event]] = new TaskToRepository(mockRepository)
+    protected val handler: Handler[EventEnvelope[Event]] = new TaskToRepository(mockRepository)
 
-  private def envelopeSource(
-    events: Seq[Event],
-    persistenceId: String,
-    startOffset: Long = 0
-  ): Source[EventEnvelope[Event], NotUsed] =
-    Source(events).zipWithIndex.map { case (event, idx) =>
-      val offset = startOffset + idx
-      EventEnvelope(
-        offset = Offset.sequence(offset),
-        persistenceId = persistenceId,
-        sequenceNr = offset,
-        event = event,
-        timestamp = offset
+    protected def envelopeSource(
+      events: Seq[Event],
+      persistenceId: String,
+      startOffset: Long = 0
+    ): Source[EventEnvelope[Event], NotUsed] =
+      Source(events).zipWithIndex.map { case (event, idx) =>
+        val offset = startOffset + idx
+        EventEnvelope(
+          offset = Offset.sequence(offset),
+          persistenceId = persistenceId,
+          sequenceNr = offset,
+          event = event,
+          timestamp = offset
+        )
+      }
+
+    protected def providerFromSource(
+      source: Source[EventEnvelope[Event], NotUsed]
+    ): TestSourceProvider[Offset, EventEnvelope[Event]] =
+      TestSourceProvider(
+        source,
+        (envelope: EventEnvelope[Event]) => envelope.offset
       )
+
+    protected def projectionFromSourceProvider(
+      sourceProvider: SourceProvider[Offset, EventEnvelope[Event]]
+    ): TestProjection[Offset, EventEnvelope[Event]] =
+      TestProjection(
+        projectionId = ProjectionId("dev-state-test", "0"),
+        sourceProvider = sourceProvider,
+        handler = () => handler
+      )
+
+    protected def projectionFromEvents(
+      events: Seq[Event],
+      persistenceId: String
+    ): TestProjection[Offset, EventEnvelope[Event]] = {
+      val source = envelopeSource(events, persistenceId)
+      projectionFromSource(source)
     }
 
-  private def providerFromSource(
-    source: Source[EventEnvelope[Event], NotUsed]
-  ): TestSourceProvider[Offset, EventEnvelope[Event]] =
-    TestSourceProvider(
-      source,
-      (envelope: EventEnvelope[Event]) => envelope.offset
-    )
+    protected def projectionFromSource(
+      source: Source[EventEnvelope[Event], NotUsed]
+    ): TestProjection[Offset, EventEnvelope[Event]] = {
+      val sourceProvider = providerFromSource(source)
+      projectionFromSourceProvider(sourceProvider)
+    }
 
-  private def projectionFromSourceProvider(
-    sourceProvider: SourceProvider[Offset, EventEnvelope[Event]]
-  ): TestProjection[Offset, EventEnvelope[Event]] =
-    TestProjection(
-      projectionId = ProjectionId("dev-state-test", "0"),
-      sourceProvider = sourceProvider,
-      handler = () => handler
-    )
-
-  private def projectionFromEvents(
-    events: Seq[Event],
-    persistenceId: String
-  ): TestProjection[Offset, EventEnvelope[Event]] = {
-    val source = envelopeSource(events, persistenceId)
-    projectionFromSource(source)
-  }
-
-  private def projectionFromSource(
-    source: Source[EventEnvelope[Event], NotUsed]
-  ): TestProjection[Offset, EventEnvelope[Event]] = {
-    val sourceProvider = providerFromSource(source)
-    projectionFromSourceProvider(sourceProvider)
   }
 
   /** В начале работы над задачей информация о текущем статусе должна сохраняться в репозиторий */
-  "The current task state" should "be saved to the repository when the task is started" in {
+  "The current task state" should "be saved to the repository when the task is started" in new Test {
     val taskWithId = TaskWithId(Task(90), UUID.randomUUID())
     val taskInfo = TaskInfo(taskWithId.id, taskWithId.task.difficulty, TaskStatus.InProgress)
     val events = Event.TaskStarted(taskWithId) :: Nil
