b592c453b387cc7519c6bebeeea003ae78ea34b7
Andrey S <norwegianwood409@gmail.com>
2022-11-17 14:53:16 +0500

    [Green] Запрос информации о задаче должен делегироваться репозиторию

--- src/main/scala/rr64/developer/infrastructure/task/TasksFromRepository.scala
+++ src/main/scala/rr64/developer/infrastructure/task/TasksFromRepository.scala
@@ -6,6 +6,6 @@ import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
 class TasksFromRepository(repository: TaskRepository) extends Tasks {
-  override def findById(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]] = ???
+  override def findById(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]] = repository.findById(id)
   override def list(implicit ec: ExecutionContext): Future[Seq[TaskInfo]] = ???
 }
