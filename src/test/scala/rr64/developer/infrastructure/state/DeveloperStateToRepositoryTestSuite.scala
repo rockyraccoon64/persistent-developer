c5ee60a478719f8863093d2a2eeaf871a74f7725
Andrey S <norwegianwood409@gmail.com>
2022-11-17 18:02:44 +0500

    [Refactor] Фикстура для тестов проекции состояния разработчика

--- src/test/scala/rr64/developer/infrastructure/state/DeveloperStateToRepositoryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/state/DeveloperStateToRepositoryTestSuite.scala
@@ -27,49 +27,53 @@ class DeveloperStateToRepositoryTestSuite
   private val projectionTestKit = ProjectionTestKit(system)
   private implicit val ec: ExecutionContext = system.executionContext
 
-  private val mockRepository: DeveloperStateRepository = new DeveloperStateRepository { // TODO Изоляция
-    private var states: Map[String, DeveloperState] = Map.empty
-    override def save(id: String, state: DeveloperState)(implicit ec: ExecutionContext): Future[Unit] = {
-      states = states.updated(id, state)
-      Future.unit
-    }
-    override def findById(id: String)(implicit ec: ExecutionContext): Future[Option[DeveloperState]] =
-      Future.successful(states.get(id))
-  }
+  trait Fixture {
 
-  private val handler: Handler[EventEnvelope[Event]] = new DeveloperStateToRepository(mockRepository)
+    private val mockRepository: DeveloperStateRepository = new DeveloperStateRepository {
+      private var states: Map[String, DeveloperState] = Map.empty
+      override def save(id: String, state: DeveloperState)(implicit ec: ExecutionContext): Future[Unit] = {
+        states = states.updated(id, state)
+        Future.unit
+      }
+      override def findById(id: String)(implicit ec: ExecutionContext): Future[Option[DeveloperState]] =
+        Future.successful(states.get(id))
+    }
+
+    private val handler: Handler[EventEnvelope[Event]] = new DeveloperStateToRepository(mockRepository)
+
+    protected def projectionFromEvents(
+      events: Seq[Event],
+      persistenceId: String = defaultPersistenceId
+    ): TestProjection[Offset, EventEnvelope[Event]] = {
+      val source = ProjectionTestUtils.envelopeSource(events, persistenceId)
+      projectionFromSource(source)
+    }
+
+    protected def projectionFromSource(
+      source: Source[EventEnvelope[Event], NotUsed]
+    ): TestProjection[Offset, EventEnvelope[Event]] =
+      TestProjection(
+        projectionId = ProjectionId("dev-proj-test", "0"),
+        sourceProvider = ProjectionTestUtils.providerFromSource(source),
+        handler = () => handler
+      )
+
+    protected def assertState(
+      state: DeveloperState,
+      persistenceId: String = defaultPersistenceId,
+      repository: DeveloperStateRepository = mockRepository
+    ): Assertion =
+      repository.findById(persistenceId).futureValue shouldEqual Some(state)
+
+  }
 
   private val defaultPersistenceId = "test-id"
 
   private val defaultTask1 = TaskWithId(Task(1), UUID.randomUUID())
   private val defaultTask2 = TaskWithId(Task(5), UUID.randomUUID())
 
-  private def projectionFromEvents(
-    events: Seq[Event],
-    persistenceId: String = defaultPersistenceId
-  ): TestProjection[Offset, EventEnvelope[Event]] = {
-    val source = ProjectionTestUtils.envelopeSource(events, persistenceId)
-    projectionFromSource(source)
-  }
-
-  private def projectionFromSource(
-    source: Source[EventEnvelope[Event], NotUsed]
-  ): TestProjection[Offset, EventEnvelope[Event]] =
-    TestProjection(
-      projectionId = ProjectionId("dev-proj-test", "0"),
-      sourceProvider = ProjectionTestUtils.providerFromSource(source),
-      handler = () => handler
-    )
-
-  def assertState(
-    state: DeveloperState,
-    persistenceId: String = defaultPersistenceId,
-    repository: DeveloperStateRepository = mockRepository
-  ): Assertion =
-    repository.findById(persistenceId).futureValue shouldEqual Some(state)
-
   /** Обработчик проекции должен обновлять состояние разработчика на "Работает", когда он начинает задачу */
-  "The handler" should "update the developer state in the repository when a task is started" in {
+  "The handler" should "update the developer state in the repository when a task is started" in new Fixture {
     val events = Event.TaskStarted(defaultTask1) :: Nil
     val projection = projectionFromEvents(events)
 
@@ -79,7 +83,7 @@ class DeveloperStateToRepositoryTestSuite
   }
 
   /** Обработчик проекции должен обновлять состояние разработчика на "Отдых", когда он заканчивает задачу */
-  "The handler" should "update the developer state in the repository when a task is finished" in {
+  "The handler" should "update the developer state in the repository when a task is finished" in new Fixture {
     val events = Event.TaskStarted(defaultTask1) ::
       Event.TaskFinished :: Nil
     val projection = projectionFromEvents(events)
@@ -91,7 +95,7 @@ class DeveloperStateToRepositoryTestSuite
 
   /** Обработчик проекции должен обновлять состояние разработчика на "Свободен",
    * когда он отдохнул и у него нет задач */
-  "The handler" should "update the state to Free after the developer rests if he has no more tasks" in {
+  "The handler" should "update the state to Free after the developer rests if he has no more tasks" in new Fixture {
     val events = Event.TaskStarted(defaultTask1) ::
       Event.TaskFinished ::
       Event.Rested ::
@@ -104,7 +108,7 @@ class DeveloperStateToRepositoryTestSuite
   }
 
   /** Обработчик проекции не должен обновлять состояние разработчика при получении задачи, когда он работает */
-  "The handler" should "not update the state after receiving a new task while working" in {
+  "The handler" should "not update the state after receiving a new task while working" in new Fixture {
     val events = Event.TaskStarted(defaultTask1) ::
       Event.TaskQueued(defaultTask2) ::
       Nil
@@ -116,7 +120,7 @@ class DeveloperStateToRepositoryTestSuite
   }
 
   /** Обработчик проекции не должен обновлять состояние разработчика при получении задачи, когда он отдыхает */
-  "The handler" should "not update the state when the developer receives a new task while resting" in {
+  "The handler" should "not update the state when the developer receives a new task while resting" in new Fixture {
     val events = Event.TaskStarted(defaultTask1) ::
       Event.TaskFinished ::
       Event.TaskQueued(defaultTask2) ::
@@ -129,7 +133,7 @@ class DeveloperStateToRepositoryTestSuite
   }
 
   /** Для каждого разработчика состояние обновляется отдельно */
-  "The handler" should "update states for different developers separately" in {
+  "The handler" should "update states for different developers separately" in new Fixture {
     val differentPersistenceId = "test-id2"
     val events1 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask2) :: Event.TaskFinished :: Nil, defaultPersistenceId)
     val events2 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask1) :: Nil, differentPersistenceId, startOffset = 2)
