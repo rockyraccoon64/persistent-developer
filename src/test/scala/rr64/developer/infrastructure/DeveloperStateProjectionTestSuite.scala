0142959a7749cf1b8f237e1a350ef4b627a8a1b6
Andrey S <norwegianwood409@gmail.com>
2022-11-17 08:51:55 +0500

    [Red] Обработчик проекции должен обновлять состояние разработчика, когда он начинает задачу

--- src/test/scala/rr64/developer/infrastructure/DeveloperStateProjectionTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/DeveloperStateProjectionTestSuite.scala
@@ -9,16 +9,19 @@ import akka.projection.scaladsl.Handler
 import akka.projection.testkit.scaladsl.{ProjectionTestKit, TestProjection, TestSourceProvider}
 import akka.stream.scaladsl.Source
 import org.scalatest.flatspec.AnyFlatSpecLike
-import rr64.developer.domain.Task
+import rr64.developer.domain.{DeveloperState, Task}
 import rr64.developer.infrastructure.DeveloperBehavior.{Event, TaskWithId}
+import rr64.developer.infrastructure.state.{DeveloperStateRepository, DeveloperStateToRepository}
 
 import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
 
 class DeveloperStateProjectionTestSuite
   extends ScalaTestWithActorTestKit
   with AnyFlatSpecLike {
 
   private val projectionTestKit = ProjectionTestKit(system)
+  private implicit val ec: ExecutionContext = system.executionContext
 
   /** Обработчик проекции должен обновлять состояние разработчика, когда он начинает задачу */
   "The handler" should "update the developer state in the repository when a task is started" in {
@@ -41,8 +44,11 @@ class DeveloperStateProjectionTestSuite
       (envelope: EventEnvelope[Event]) => envelope.offset
     )
 
-    val mockRepo = new DeveloperStateRepository
-    val handler: Handler[Event] = new DeveloperStateToRepository(mockRepo)
+    val mockRepo = new DeveloperStateRepository {
+      override def save(id: String, state: DeveloperState)(implicit ec: ExecutionContext): Future[Unit] = ???
+      override def findById(id: String)(implicit ec: ExecutionContext): Future[Option[DeveloperState]] = ???
+    }
+    val handler: Handler[EventEnvelope[Event]] = new DeveloperStateToRepository(mockRepo)
 
     val projection = TestProjection(ProjectionId("dev-state-test", "0"), sourceProvider, () => handler)
 
