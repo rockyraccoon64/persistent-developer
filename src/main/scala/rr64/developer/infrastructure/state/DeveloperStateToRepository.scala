4e956debf07e5f4fd829a144a736ffd7b45f5290
Andrey S <norwegianwood409@gmail.com>
2022-11-17 09:15:01 +0500

    [Refactor] Комментарии к обработчику проекции

--- src/main/scala/rr64/developer/infrastructure/state/DeveloperStateToRepository.scala
+++ src/main/scala/rr64/developer/infrastructure/state/DeveloperStateToRepository.scala
@@ -8,9 +8,13 @@ import rr64.developer.infrastructure.DeveloperBehavior.Event
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Обработчик проекции для сохранения состояния разработчика в репозитории
+ * */
 class DeveloperStateToRepository(repository: DeveloperStateRepository)
     (implicit ec: ExecutionContext) extends Handler[EventEnvelope[Event]] {
 
+  /** Обработать событие */
   override def process(envelope: EventEnvelope[Event]): Future[Done] = envelope.event match {
     case Event.TaskStarted(_) =>
       save(envelope.persistenceId, DeveloperState.Working)
@@ -18,6 +22,7 @@ class DeveloperStateToRepository(repository: DeveloperStateRepository)
       save(envelope.persistenceId, DeveloperState.Resting)
   }
 
+  /** Сохранить состояние разработчика в репозитории */
   private def save(id: String, state: DeveloperState): Future[Done] = {
     repository.save(id, state).map(_ => Done)
   }
