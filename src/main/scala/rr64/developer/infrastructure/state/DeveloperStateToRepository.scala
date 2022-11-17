ec65d31be7556cb59aaf168d259d14367a616551
Andrey S <norwegianwood409@gmail.com>
2022-11-17 09:03:08 +0500

    [Green] Обновление состояния разработчика в проекции при старте задачи

--- src/main/scala/rr64/developer/infrastructure/state/DeveloperStateToRepository.scala
+++ src/main/scala/rr64/developer/infrastructure/state/DeveloperStateToRepository.scala
@@ -3,10 +3,15 @@ package rr64.developer.infrastructure.state
 import akka.Done
 import akka.projection.eventsourced.EventEnvelope
 import akka.projection.scaladsl.Handler
+import rr64.developer.domain.DeveloperState
 import rr64.developer.infrastructure.DeveloperBehavior.Event
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class DeveloperStateToRepository(repository: DeveloperStateRepository) extends Handler[EventEnvelope[Event]] {
-  override def process(envelope: EventEnvelope[Event]): Future[Done] = ???
+class DeveloperStateToRepository(repository: DeveloperStateRepository)
+    (implicit ec: ExecutionContext) extends Handler[EventEnvelope[Event]] {
+  override def process(envelope: EventEnvelope[Event]): Future[Done] = envelope.event match {
+    case Event.TaskStarted(_) =>
+      repository.save(envelope.persistenceId, DeveloperState.Working).map(_ => Done)
+  }
 }
