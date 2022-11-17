2132db06ff5970581283d3c0cd30108fc993efa9
Andrey S <norwegianwood409@gmail.com>
2022-11-17 12:51:28 +0500

    Каркас для набора тестов DAO состояний разработчиков

---
+++ src/test/scala/rr64/developer/infrastructure/state/DeveloperStateTableTestSuite.scala
@@ -0,0 +1,12 @@
+package rr64.developer.infrastructure.state
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import rr64.developer.infrastructure.PostgresSpec
+
+class DeveloperStateTableTestSuite
+  extends PostgresSpec
+    with AnyFlatSpecLike
+    with Matchers {
+
+}
