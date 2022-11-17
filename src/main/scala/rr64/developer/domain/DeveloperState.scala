1a378005e603356a6c426e109ea2c6264ad28d78
Andrey S <norwegianwood409@gmail.com>
2022-11-17 09:11:26 +0500

    [Red] Обработчик проекции должен обновлять состояние разработчика на "Отдых", когда он заканчивает задачу

--- src/main/scala/rr64/developer/domain/DeveloperState.scala
+++ src/main/scala/rr64/developer/domain/DeveloperState.scala
@@ -5,4 +5,5 @@ sealed trait DeveloperState
 object DeveloperState {
   case object Free extends DeveloperState
   case object Working extends DeveloperState
+  case object Resting extends DeveloperState
 }
