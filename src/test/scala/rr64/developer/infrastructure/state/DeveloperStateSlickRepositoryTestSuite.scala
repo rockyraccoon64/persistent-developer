6e6537c980188404b344f9b5054a84090fd79dbb
Andrey S <norwegianwood409@gmail.com>
2022-11-17 13:40:27 +0500

    [Compile Error] Состояние "Свободен" должно добавляться и извлекаться из репозитория

--- src/test/scala/rr64/developer/infrastructure/state/DeveloperStateSlickRepositoryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/state/DeveloperStateSlickRepositoryTestSuite.scala
@@ -1,8 +1,22 @@
 package rr64.developer.infrastructure.state
 
+import org.scalatest.flatspec.AsyncFlatSpecLike
 import org.scalatest.matchers.should.Matchers
+import rr64.developer.domain.DeveloperState
 import rr64.developer.infrastructure.PostgresSpec
 
-class DeveloperStateSlickRepositoryTestSuite extends PostgresSpec with Matchers {
+class DeveloperStateSlickRepositoryTestSuite extends PostgresSpec with AsyncFlatSpecLike with Matchers {
+
+  private val repository = new DeveloperStateSlickRepository(database)
+
+  /** Состояние "Свободен" должно добавляться и извлекаться из репозитория */
+  "The free developer state" should "be inserted" in {
+    for {
+      _ <- repository.save("dev-1", DeveloperState.Free)
+      state <- repository.findById("dev-1")
+    } yield {
+      state shouldEqual Some(DeveloperState.Free)
+    }
+  }
 
 }
