c988decd5a3f727f8fdc9cb26b21033700736f3a
Andrey S <norwegianwood409@gmail.com>
2022-11-17 13:34:49 +0500

    Каркас для тестов репозитория состояний разработчиков

---
+++ src/test/scala/rr64/developer/infrastructure/state/DeveloperStateSlickRepositoryTestSuite.scala
@@ -0,0 +1,8 @@
+package rr64.developer.infrastructure.state
+
+import org.scalatest.matchers.should.Matchers
+import rr64.developer.infrastructure.PostgresSpec
+
+class DeveloperStateSlickRepositoryTestSuite extends PostgresSpec with Matchers {
+
+}
