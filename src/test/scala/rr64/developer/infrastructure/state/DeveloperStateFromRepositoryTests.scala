793e02fef2ccece35cf7cc9fe96882b8b88688c8
Andrey S <norwegianwood409@gmail.com>
2022-11-17 12:00:31 +0500

    [Green] Тест: Если состояние разработчика не сохранено в репозитории, он ещё не получил задачу и находится в начальном состоянии

--- src/test/scala/rr64/developer/infrastructure/state/DeveloperStateFromRepositoryTests.scala
+++ src/test/scala/rr64/developer/infrastructure/state/DeveloperStateFromRepositoryTests.scala
@@ -6,7 +6,7 @@ import rr64.developer.domain.DeveloperState
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class DeveloperStateFromRepositoryTests extends AsyncFlatSpec with Matchers { // TODO В пакет state
+class DeveloperStateFromRepositoryTests extends AsyncFlatSpec with Matchers {
 
   /** Источник должен извлекать известное состояние разработчика из репозитория */
   "The provider" should "extract an existing state for the given developer id from the repository" in {
@@ -27,6 +27,28 @@ class DeveloperStateFromRepositoryTests extends AsyncFlatSpec with Matchers { //
     val provider = new DeveloperStateFromRepository(developerId, mockRepository)
 
     provider.state.map(_ shouldEqual DeveloperState.Working)
+  }
+
+  /** Если состояние разработчика не сохранено в репозитории,
+   * он ещё не получил задачу и находится в начальном состоянии */
+  "The provider" should "return the initial state if the developer's state is not saved in the repository" in {
+    val developerId = "dev-id1"
+    val mockRepository = new DeveloperStateRepository {
+      private var states: Map[String, DeveloperState] = Map(
+        developerId -> DeveloperState.Working,
+        "dev-id2" -> DeveloperState.Resting,
+        "dev-id3" -> DeveloperState.Free
+      )
+      override def save(id: String, state: DeveloperState)(implicit ec: ExecutionContext): Future[Unit] = {
+        states = states.updated(id, state)
+        Future.unit
+      }
+      override def findById(id: String)(implicit ec: ExecutionContext): Future[Option[DeveloperState]] =
+        Future.successful(states.get(id))
+    }
+    val provider = new DeveloperStateFromRepository("dev-id4", mockRepository)
+
+    provider.state.map(_ shouldEqual DeveloperState.Free)
   }
 
 }
