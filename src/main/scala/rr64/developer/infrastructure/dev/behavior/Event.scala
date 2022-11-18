66cab6c36e8e5372d6ca3042f5c2537a4337927d
Andrey S <norwegianwood409@gmail.com>
2022-11-18 13:43:34 +0500

    [Refactor] Комментарии к событиям

--- src/main/scala/rr64/developer/infrastructure/dev/behavior/Event.scala
+++ src/main/scala/rr64/developer/infrastructure/dev/behavior/Event.scala
@@ -2,10 +2,18 @@ package rr64.developer.infrastructure.dev.behavior
 
 import rr64.developer.infrastructure.task.TaskWithId
 
+/**
+ * Событие актора разработчика
+ * */
 sealed trait Event
+
 object Event {
+  /** Задача принята в разработку */
   case class TaskStarted(taskWithId: TaskWithId) extends Event
+  /** Задача поставлена в очередь */
   case class TaskQueued(taskWithId: TaskWithId) extends Event
+  /** Задача завершена */
   case class TaskFinished(taskWithId: TaskWithId) extends Event
+  /** Отдых завершён */
   case object Rested extends Event
 }
