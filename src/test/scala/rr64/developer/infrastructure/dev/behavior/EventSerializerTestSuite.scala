93b65f59d98579ad68c6e085757c59b0df927219
Andrey S <norwegianwood409@gmail.com>
2022-11-18 19:57:36 +0500

    [Refactor] Устранение дублирования в тестах сериализации

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
@@ -11,20 +11,22 @@ class EventSerializerTestSuite extends AnyFlatSpec with Matchers {
 
   private val serializer = new EventSerializer
 
-  "The TaskQueued event" should "be serialized" in {
-    val task = TaskWithId(Task(3), UUID.randomUUID())
-    val event = Event.TaskQueued(task)
+  private def assertSerialized(event: Event) {
     val bytes = serializer.toBinary(event)
     val manifest = serializer.manifest(event)
     serializer.fromBinary(bytes, manifest) shouldEqual event
   }
 
+  "The TaskQueued event" should "be serialized" in {
+    val task = TaskWithId(Task(3), UUID.randomUUID())
+    val event = Event.TaskQueued(task)
+    assertSerialized(event)
+  }
+
   "The TaskStarted event" should "be serialized" in {
     val task = TaskWithId(Task(3), UUID.randomUUID())
     val event = Event.TaskStarted(task)
-    val bytes = serializer.toBinary(event)
-    val manifest = serializer.manifest(event)
-    serializer.fromBinary(bytes, manifest) shouldEqual event
+    assertSerialized(event)
   }
 
 }
