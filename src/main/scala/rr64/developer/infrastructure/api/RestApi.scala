f2aebd5a7dc08c9e5117f34e4c9908601a9c4a48
Andrey S <norwegianwood409@gmail.com>
2022-11-18 16:25:08 +0500

    [Refactor] Конвертация состояния разработчика в API

--- src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
+++ src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
@@ -4,12 +4,15 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import rr64.developer.domain.DeveloperService
+import rr64.developer.domain.{DeveloperService, DeveloperState}
 import rr64.developer.infrastructure.api.ApiDeveloperState._
 
 import scala.util.{Failure, Success}
 
 class RestApi(service: DeveloperService) {
+
+  private val developerStateAdapter = implicitly[Adapter[DeveloperState, ApiDeveloperState]]
+
   val route: Route =
     pathPrefix("api") {
       pathPrefix("query") {
@@ -17,7 +20,7 @@ class RestApi(service: DeveloperService) {
           get {
             extractExecutionContext { implicit exec =>
               onComplete(service.developerState) {
-                case Success(value) => complete(ApiDeveloperState.Working)
+                case Success(value) => complete(developerStateAdapter.convert(value))
                 case Failure(exception) => complete(StatusCodes.InternalServerError)
               }
             }
@@ -25,4 +28,5 @@ class RestApi(service: DeveloperService) {
         }
       }
     }
+
 }
