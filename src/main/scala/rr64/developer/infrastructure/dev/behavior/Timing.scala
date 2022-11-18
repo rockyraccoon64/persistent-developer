568533f7aa84baa41ee7b3a874b4f3c3719b739e
Andrey S <norwegianwood409@gmail.com>
2022-11-18 13:49:57 +0500

    [Refactor] Комментарии к вспомогательным методам для таймеров

--- src/main/scala/rr64/developer/infrastructure/dev/behavior/Timing.scala
+++ src/main/scala/rr64/developer/infrastructure/dev/behavior/Timing.scala
@@ -5,11 +5,17 @@ import rr64.developer.infrastructure.task.TaskWithId
 
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
+/**
+ * Вспомогательные методы для таймеров
+ * актора разработчика
+ * */
 object Timing {
 
+  /** Расчитать время таймера, исходя из сложности задачи и множителя */
   def calculateTime(difficulty: Int, factor: Int): FiniteDuration =
     (difficulty * factor).millis
 
+  /** Запустить таймер до завершения задачи */
   private[behavior] def startWorkTimer(taskWithId: TaskWithId)
       (implicit setup: Setup): Unit =
     startTimer(
@@ -19,6 +25,7 @@ object Timing {
       factor = setup.workFactor
     )
 
+  /** Запустить таймер до завершения отдыха */
   private[behavior] def startRestTimer(taskWithId: TaskWithId)
       (implicit setup: Setup): Unit =
     startTimer(
@@ -28,6 +35,7 @@ object Timing {
       factor = setup.restFactor
     )
 
+  /** Запустить таймер, исходя из сложности задачи и множителя */
   private def startTimer(
     timer: TimerScheduler[Command],
     message: Command,
