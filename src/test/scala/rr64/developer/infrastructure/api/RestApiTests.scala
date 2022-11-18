d26b06c2a2719d562818f45d3995dc00eb231ac7
Andrey S <norwegianwood409@gmail.com>
2022-11-18 16:27:05 +0500

    [Refactor] ScalaMock для сервиса в тестах API

--- src/test/scala/rr64/developer/infrastructure/api/RestApiTests.scala
+++ src/test/scala/rr64/developer/infrastructure/api/RestApiTests.scala
@@ -2,25 +2,20 @@ package rr64.developer.infrastructure.api
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import rr64.developer.domain._
 
-import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
 class RestApiTests
   extends AnyWordSpec
     with Matchers
-    with ScalatestRouteTest {
+    with ScalatestRouteTest
+    with MockFactory {
 
-  private val service = new DeveloperService {
-    override def addTask(task: Task)(implicit ec: ExecutionContext): Future[DeveloperReply] = ???
-    override def developerState(implicit ec: ExecutionContext): Future[DeveloperState] =
-      Future.successful(DeveloperState.Working)
-    override def taskInfo(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]] = ???
-    override def tasks(implicit ec: ExecutionContext): Future[Seq[TaskInfo]] = ???
-  }
+  private val service = mock[DeveloperService]
 
   private val route = new RestApi(service).route
 
@@ -28,6 +23,11 @@ class RestApiTests
 
     /** Запрос состояния разработчика, когда он работает */
     "return the Working developer state" in {
+      (service.developerState(_: ExecutionContext))
+        .expects(*)
+        .returning(
+          Future.successful(DeveloperState.Working)
+        )
       Get("/api/query/developer-state") ~> route ~> check {
         responseAs[ApiDeveloperState] shouldEqual ApiDeveloperState.Working
       }
