052bf14bf34aa8cd0c1d35bb81bc7743fb0c6573
Andrey S <norwegianwood409@gmail.com>
2022-11-18 17:52:30 +0500

    [Refactor] Устранение дублирования в обработчиках ошибок

--- src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
+++ src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
@@ -7,8 +7,6 @@ import akka.http.scaladsl.server.Route
 import rr64.developer.domain.{DeveloperService, DeveloperState, TaskInfo}
 import rr64.developer.infrastructure.api.ApiDeveloperState._
 
-import scala.util.{Failure, Success}
-
 class RestApi(service: DeveloperService) {
 
   private val developerStateAdapter = implicitly[Adapter[DeveloperState, ApiDeveloperState]]
@@ -18,17 +16,15 @@ class RestApi(service: DeveloperService) {
     pathPrefix("api") {
       (pathPrefix("query") & get) {
         (path("developer-state") & extractExecutionContext) { implicit exec =>
-          onComplete(service.developerState) {
-            case Success(value) => complete(developerStateAdapter.convert(value))
-            case Failure(exception) => complete(StatusCodes.InternalServerError)
+          onSuccess(service.developerState) { value =>
+            complete(developerStateAdapter.convert(value))
           }
         } ~
         path("task-info" / JavaUUID) { id =>
           extractExecutionContext { implicit exec =>
-            onComplete(service.taskInfo(id)) {
-              case Success(Some(value)) => complete(taskInfoAdapter.convert(value))
-              case Success(None) => complete(StatusCodes.NotFound)
-              case Failure(exception) => complete(StatusCodes.InternalServerError)
+            onSuccess(service.taskInfo(id)) {
+              case Some(value) => complete(taskInfoAdapter.convert(value))
+              case None => complete(StatusCodes.NotFound)
             }
           }
         }
