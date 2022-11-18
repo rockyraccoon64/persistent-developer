bb0a55fe4d02c0b1ee0f495516d6e6fc1be00001
Andrey S <norwegianwood409@gmail.com>
2022-11-18 16:22:24 +0500

    [Refactor] Адаптер для состояния разработчика

--- src/main/scala/rr64/developer/infrastructure/api/ApiDeveloperState.scala
+++ src/main/scala/rr64/developer/infrastructure/api/ApiDeveloperState.scala
@@ -1,5 +1,6 @@
 package rr64.developer.infrastructure.api
 
+import rr64.developer.domain.DeveloperState
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
@@ -7,9 +8,17 @@ case class ApiDeveloperState private(state: String)
 
 object ApiDeveloperState {
 
+  val Free: ApiDeveloperState = ApiDeveloperState("Free")
   val Working: ApiDeveloperState = ApiDeveloperState("Working")
+  val Resting: ApiDeveloperState = ApiDeveloperState("Resting")
 
   implicit val developerStateJsonFormat: RootJsonFormat[ApiDeveloperState] =
     jsonFormat1(ApiDeveloperState.apply)
+
+  implicit val adapter: Adapter[DeveloperState, ApiDeveloperState] = {
+    case DeveloperState.Free => Free
+    case DeveloperState.Working => Working
+    case DeveloperState.Resting => Resting
+  }
 
 }
