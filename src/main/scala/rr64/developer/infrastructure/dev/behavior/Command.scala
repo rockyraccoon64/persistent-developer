196e1e5409f3f9fd06b8b5958617d131a732b2bf
Andrey S <norwegianwood409@gmail.com>
2022-11-18 13:44:10 +0500

    [Refactor] Комментарии к командам

--- src/main/scala/rr64/developer/infrastructure/dev/behavior/Command.scala
+++ src/main/scala/rr64/developer/infrastructure/dev/behavior/Command.scala
@@ -5,10 +5,16 @@ import rr64.developer.domain.Task
 
 import java.util.UUID
 
+/**
+ * Команда актору разработчика
+ */
 sealed trait Command
 
 object Command {
+  /** Добавить задачу */
   case class AddTask(task: Task, replyTo: ActorRef[Replies.AddTaskResult]) extends Command
+  /** Завершить задачу */
   private[behavior] case class FinishTask(id: UUID) extends Command
+  /** Завершить отдых */
   private[behavior] case object StopResting extends Command
 }
