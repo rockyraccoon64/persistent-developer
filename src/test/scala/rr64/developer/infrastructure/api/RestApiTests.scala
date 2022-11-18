7eb5ceb34154df8b65e24694a1999767537645ce
Andrey S <norwegianwood409@gmail.com>
2022-11-18 16:42:21 +0500

    [Compile Error] Тест: Запрос информации о существующей задаче

--- src/test/scala/rr64/developer/infrastructure/api/RestApiTests.scala
+++ src/test/scala/rr64/developer/infrastructure/api/RestApiTests.scala
@@ -8,6 +8,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import rr64.developer.domain._
 
+import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
 class RestApiTests
@@ -37,6 +38,24 @@ class RestApiTests
       checkState(DeveloperState.Working, ApiDeveloperState.Working)
       checkState(DeveloperState.Resting, ApiDeveloperState.Resting)
     }
+
+    /** TODO При ошибке при запросе состояния разработчика возвращается 500 */
+
+    /** Запрос информации о существующей задаче */
+    "return the task info" in {
+      val id = UUID.fromString("6f9ed143-70f4-4406-9c6b-2d9ddd297304")
+      val taskInfo = TaskInfo(id, 35, TaskStatus.InProgress)
+      (service.taskInfo(_)(_: ExecutionContext))
+        .expects(id, *)
+        .returning(
+          Future.successful(Some(taskInfo))
+        )
+      Get(s"/api/query/task-info/${id.toString}") ~> route ~> check {
+        responseAs[ApiTaskInfo] shouldEqual ApiTaskInfo(id, 35, ApiTaskStatus.InProgress)
+      }
+    }
+
+    /** TODO При ошибке в UUID запроса информации о задаче возвращается ??? */
 
   }
 
