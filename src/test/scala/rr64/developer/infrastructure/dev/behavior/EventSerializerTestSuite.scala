e3145bb8672ac40c62def9641961361faacca2f8
Andrey S <norwegianwood409@gmail.com>
2022-11-18 19:59:47 +0500

    [Refactor] Переименование тестов

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
@@ -16,13 +16,13 @@ class EventSerializerTestSuite extends AnyFlatSpec with Matchers {
     serializer.fromBinary(bytes, manifest) shouldEqual event
   }
 
-  "The TaskQueued event" should "be serialized" in {
+  "The Task Queued event" should "be serialized" in {
     val task = TaskWithId(3, UUID.fromString("3b93d086-16f1-410e-9325-4c2e220bef8f"))
     val event = Event.TaskQueued(task)
     assertSerialized(event)
   }
 
-  "The TaskStarted event" should "be serialized" in {
+  "The Task Started event" should "be serialized" in {
     val task = TaskWithId(19, UUID.fromString("39ea0ce2-e56f-42a8-aa4c-0a3cd496894e"))
     val event = Event.TaskStarted(task)
     assertSerialized(event)
