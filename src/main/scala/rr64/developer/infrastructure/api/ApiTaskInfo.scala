6e90158f88e6c73ca8be3da96e11dbb0e209a019
Andrey S <norwegianwood409@gmail.com>
2022-11-18 16:54:54 +0500

    [Green] Запрос информации о существующей задаче

--- src/main/scala/rr64/developer/infrastructure/api/ApiTaskInfo.scala
+++ src/main/scala/rr64/developer/infrastructure/api/ApiTaskInfo.scala
@@ -1,5 +1,6 @@
 package rr64.developer.infrastructure.api
 
+import rr64.developer.domain.{TaskInfo, TaskStatus}
 import rr64.developer.infrastructure.api.CommonJsonFormats._
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
@@ -11,4 +12,20 @@ case class ApiTaskInfo(id: UUID, difficulty: Int, status: String)
 object ApiTaskInfo {
   implicit val apiTaskInfoJsonFormat: RootJsonFormat[ApiTaskInfo] =
     jsonFormat3(ApiTaskInfo.apply)
+
+  implicit val adapter: Adapter[TaskInfo, ApiTaskInfo] = value => {
+    val TaskInfo(
+      id: UUID,
+      difficulty: Int,
+      status: TaskStatus
+    ) = value
+
+    val statusString = status match {
+      case TaskStatus.InProgress => "InProgress"
+      case TaskStatus.Queued => "Queued"
+      case TaskStatus.Finished => "Finished"
+    }
+
+    ApiTaskInfo(id, difficulty, statusString)
+  }
 }
--- src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
+++ src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import rr64.developer.domain.{DeveloperService, DeveloperState}
+import rr64.developer.domain.{DeveloperService, DeveloperState, TaskInfo}
 import rr64.developer.infrastructure.api.ApiDeveloperState._
 
 import scala.util.{Failure, Success}
@@ -12,6 +12,7 @@ import scala.util.{Failure, Success}
 class RestApi(service: DeveloperService) {
 
   private val developerStateAdapter = implicitly[Adapter[DeveloperState, ApiDeveloperState]]
+  private val taskInfoAdapter = implicitly[Adapter[TaskInfo, ApiTaskInfo]]
 
   val route: Route =
     pathPrefix("api") {
@@ -23,6 +24,14 @@ class RestApi(service: DeveloperService) {
                 case Success(value) => complete(developerStateAdapter.convert(value))
                 case Failure(exception) => complete(StatusCodes.InternalServerError)
               }
+            }
+          }
+        } ~
+        (path("task-info" / JavaUUID) & get) { id =>
+          extractExecutionContext { implicit exec =>
+            onComplete(service.taskInfo(id)) {
+              case Success(value) => complete(value.map(taskInfoAdapter.convert))
+              case Failure(exception) => complete(StatusCodes.InternalServerError)
             }
           }
         }
