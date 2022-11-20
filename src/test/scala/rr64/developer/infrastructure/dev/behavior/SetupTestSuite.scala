b2ccf9650d12037703423b7858f553d61505313a
Andrey S <norwegianwood409@gmail.com>
2022-11-20 16:25:19 +0500

    [Green] Рабочий множитель должен быть в пределах 1-1000

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
@@ -23,6 +23,9 @@ class SetupTestSuite extends AnyWordSpec with Matchers with MockFactory {
         setupFromWorkFactor(workFactor)
       }
 
+    def assertNoException(workFactor: Int): Assertion =
+      noException should be thrownBy setupFromWorkFactor(workFactor)
+
     /** Не должен быть меньше единицы */
     "not be allowed to be less than 1" in {
       assertFactorException(0)
@@ -34,6 +37,13 @@ class SetupTestSuite extends AnyWordSpec with Matchers with MockFactory {
     "not be allowed to be greater than 1000" in {
       assertFactorException(1001)
       assertFactorException(987654)
+    }
+
+    /** Должен быть в пределах 1-1000 */
+    "be allowed to be between 1 and 1000" in {
+      assertNoException(1)
+      assertNoException(445)
+      assertNoException(1000)
     }
 
   }
