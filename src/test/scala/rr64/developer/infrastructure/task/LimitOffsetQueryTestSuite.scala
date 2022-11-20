73ac0243b6915172bd7d6fc22401c3c31d96bbf7
Andrey S <norwegianwood409@gmail.com>
2022-11-20 18:52:30 +0500

    [Compile Error] Тест: Количество возвращаемых элементов не должно быть меньше одного

---
+++ src/test/scala/rr64/developer/infrastructure/task/LimitOffsetQueryTestSuite.scala
@@ -0,0 +1,24 @@
+package rr64.developer.infrastructure.task
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class LimitOffsetQueryTestSuite
+  extends AnyWordSpec
+    with Matchers {
+
+  /** Количество возвращаемых элементов */
+  "The limit" should {
+
+    /** Не должно быть меньше одного */
+    "not be allowed to be less than 1" in {
+      assertThrows[LimitOffsetException] {
+        LimitOffsetQuery(limit = 0)
+      }
+      assertThrows[LimitOffsetException] {
+        LimitOffsetQuery(limit = -1)
+      }
+    }
+  }
+
+}
