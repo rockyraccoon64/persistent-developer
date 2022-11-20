9c42919b60dea3a91259923516447f659c437fd3
Andrey S <norwegianwood409@gmail.com>
2022-11-20 16:20:02 +0500

    [Refactor] Устранение дублирования при проверке на исключение

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
@@ -2,6 +2,7 @@ package rr64.developer.infrastructure.dev.behavior
 
 import akka.actor.typed.scaladsl.TimerScheduler
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import rr64.developer.infrastructure.dev.behavior.Setup.FactorException
@@ -17,18 +18,18 @@ class SetupTestSuite extends AnyWordSpec with Matchers with MockFactory {
   /** Рабочий множитель */
   "The work factor" should {
 
+    def assertFactorException(workFactor: Int): Assertion =
+      assertThrows[FactorException] {
+        setupFromWorkFactor(workFactor)
+      }
+
     /** Не должен быть меньше единицы */
     "not be allowed to be less than 1" in {
-      assertThrows[FactorException] {
-        setupFromWorkFactor(0)
-      }
-      assertThrows[FactorException] {
-        setupFromWorkFactor(-1)
-      }
-      assertThrows[FactorException] {
-        setupFromWorkFactor(-15)
-      }
+      assertFactorException(0)
+      assertFactorException(-1)
+      assertFactorException(-15)
     }
+
   }
 
 }
