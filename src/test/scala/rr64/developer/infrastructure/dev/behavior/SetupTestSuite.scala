928de500da23af02d04ed8c6f2b0c075d46b4633
Andrey S <norwegianwood409@gmail.com>
2022-11-20 16:09:39 +0500

    [Compile Error] Тест: Рабочий множитель не должен быть меньше единицы

---
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
@@ -0,0 +1,31 @@
+package rr64.developer.infrastructure.dev.behavior
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class SetupTestSuite extends AnyWordSpec with Matchers {
+
+  private def setupFromWorkFactor(workFactor: Int) = Setup(
+    workFactor = workFactor,
+    restFactor = ???,
+    timer = ???
+  )
+
+  /** Рабочий множитель */
+  "The work factor" should {
+
+    /** Не должен быть меньше единицы */
+    "not be allowed to be less than 1" in {
+      assertThrows[FactorException] {
+        setupFromWorkFactor(0)
+      }
+      assertThrows[FactorException] {
+        setupFromWorkFactor(-1)
+      }
+      assertThrows[FactorException] {
+        setupFromWorkFactor(-15)
+      }
+    }
+  }
+
+}
