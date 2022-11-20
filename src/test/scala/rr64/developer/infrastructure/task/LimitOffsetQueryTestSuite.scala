36f7a90b58607d864903b800748b4543d49aaf10
Andrey S <norwegianwood409@gmail.com>
2022-11-20 20:27:43 +0500

    [Refactor] Фикстура для тестов limit и offset

--- src/test/scala/rr64/developer/infrastructure/task/LimitOffsetQueryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/task/LimitOffsetQueryTestSuite.scala
@@ -9,6 +9,8 @@ class LimitOffsetQueryTestSuite
   extends AnyWordSpec
     with Matchers {
 
+  private val factory = new QueryFactory(defaultLimit = 10, maxLimit = 30)
+
   trait FactoryTest {
 
     def assertIllegal(defaultLimit: Int, maxLimit: Int): Assertion = {
@@ -24,15 +26,17 @@ class LimitOffsetQueryTestSuite
 
   }
 
-  private val factory = new QueryFactory(defaultLimit = 10, maxLimit = 30)
+  trait LimitOffsetTest {
 
-  private def assertException(limit: Int = 10, offset: Int = 0): Assertion =
-    assertThrows[LimitOffsetException] {
-      factory.create(limit = limit, offset = offset)
-    }
+    def assertException(limit: Int = 10, offset: Int = 0): Assertion =
+      assertThrows[LimitOffsetException] {
+        factory.create(limit = limit, offset = offset)
+      }
 
-  private def assertNoException(limit: Int = 10, offset: Int = 0): Assertion =
-    noException should be thrownBy factory.create(limit = limit, offset = offset)
+    def assertNoException(limit: Int = 10, offset: Int = 0): Assertion =
+      noException should be thrownBy factory.create(limit = limit, offset = offset)
+
+  }
 
   /** Фабрика параметров запроса */
   "The factory" should {
@@ -65,23 +69,23 @@ class LimitOffsetQueryTestSuite
   "The limit" should {
 
     /** Не должно быть меньше одного */
-    "not be allowed to be less than 1" in {
+    "not be allowed to be less than 1" in new LimitOffsetTest {
       assertException(limit = 0)
       assertException(limit = -1)
     }
 
     /** Не должно быть больше максимального */
-    "not be allowed to be greater than the max limit" in {
+    "not be allowed to be greater than the max limit" in new LimitOffsetTest {
       assertException(limit = 31)
     }
 
     /** Может быть максимальным */
-    "be allowed to be equal to the max limit" in {
+    "be allowed to be equal to the max limit" in new LimitOffsetTest {
       assertNoException(limit = 30)
     }
 
     /** Может быть больше нуля */
-    "be allowed to be greater than zero" in {
+    "be allowed to be greater than zero" in new LimitOffsetTest {
       assertNoException(limit = 1)
       assertNoException(limit = 25)
     }
@@ -92,13 +96,13 @@ class LimitOffsetQueryTestSuite
   "The offset" should {
 
     /** Не должен быть меньше нуля */
-    "not be allowed to be less than zero" in {
+    "not be allowed to be less than zero" in new LimitOffsetTest {
       assertException(offset = -1)
       assertException(offset = -5)
     }
 
     /** Может быть больше или равен нулю */
-    "be allowed to be greater than or equal to zero" in {
+    "be allowed to be greater than or equal to zero" in new LimitOffsetTest {
       assertNoException(offset = 0)
       assertNoException(offset = 1)
       assertNoException(offset = 99)
