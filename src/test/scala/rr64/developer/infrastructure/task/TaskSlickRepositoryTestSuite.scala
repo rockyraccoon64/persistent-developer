dbecab151673158c9000ebf3b816501f1d3a30ab
Andrey S <norwegianwood409@gmail.com>
2022-11-20 18:37:53 +0500

    [Refactor] Дополнительный параметр

--- src/test/scala/rr64/developer/infrastructure/task/TaskSlickRepositoryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/task/TaskSlickRepositoryTestSuite.scala
@@ -123,9 +123,9 @@ class TaskSlickRepositoryTestSuite
     } yield taskOpt shouldEqual None
   }
 
-  def listTest(query: LimitOffsetQuery, expected: Seq[TaskInfo]): Future[Assertion] = {
+  def listTest(query: LimitOffsetQuery, initial: Seq[TaskInfo], expected: Seq[TaskInfo]): Future[Assertion] = {
     for {
-      _ <- taskList.foldLeft[Future[Any]](Future.unit) { (acc, task) =>
+      _ <- initial.foldLeft[Future[Any]](Future.unit) { (acc, task) =>
         acc.flatMap(_ => repository.save(task))
       }
       list <- repository.list(query)
@@ -137,12 +137,14 @@ class TaskSlickRepositoryTestSuite
   /** Репозиторий должен ограничить количество возвращаемых задач переданным в limit числом */
   "The repository" should "limit the number of returned tasks" in listTest(
     query = LimitOffsetQuery(limit = 2, offset = 0),
+    initial = taskList,
     expected = taskList.take(2)
   )
 
   /** Если задач меньше, чем limit, возвращаются все задачи */
   "The repository" should "return all tasks if the limit exceeds their amount" in listTest(
     query = LimitOffsetQuery(limit = 4, offset = 0),
+    initial = taskList,
     expected = taskList
   )
 
