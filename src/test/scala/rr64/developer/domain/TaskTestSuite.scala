52396206af1a2137f766b8600073e786522a579a
Andrey S <norwegianwood409@gmail.com>
2022-11-20 14:04:49 +0500

    [Red] Тест: У задач должна быть сложность больше нуля

---
+++ src/test/scala/rr64/developer/domain/TaskTestSuite.scala
@@ -0,0 +1,26 @@
+package rr64.developer.domain
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class TaskTestSuite extends AnyWordSpec with Matchers {
+
+  "Tasks" should {
+
+    /** У задач должна быть сложность больше нуля */
+    "only be allowed to have positive difficulty" in {
+      assertThrows[IllegalArgumentException] {
+        Task(0)
+      }
+      assertThrows[IllegalArgumentException] {
+        Task(-1)
+      }
+      assertThrows[IllegalArgumentException] {
+        Task(-55)
+      }
+      noException should be thrownBy Task(1)
+      noException should be thrownBy Task(5)
+    }
+  }
+
+}
