341acc059ea69f6b96e1c4785cd0d8d9e4a47cb8
Andrey S <norwegianwood409@gmail.com>
2022-11-20 19:00:43 +0500

    [Refactor] Устранение дублирования

--- src/test/scala/rr64/developer/infrastructure/task/LimitOffsetQueryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/task/LimitOffsetQueryTestSuite.scala
@@ -1,5 +1,6 @@
 package rr64.developer.infrastructure.task
 
+import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import rr64.developer.infrastructure.task.LimitOffsetQuery.LimitOffsetException
@@ -8,17 +9,18 @@ class LimitOffsetQueryTestSuite
   extends AnyWordSpec
     with Matchers {
 
+  def assertException(limit: Int = 10, offset: Int = 0): Assertion =
+    assertThrows[LimitOffsetException] {
+      LimitOffsetQuery(limit = limit, offset = offset)
+    }
+
   /** Количество запрашиваемых элементов */
   "The limit" should {
 
     /** Не должно быть меньше одного */
     "not be allowed to be less than 1" in {
-      assertThrows[LimitOffsetException] {
-        LimitOffsetQuery(limit = 0)
-      }
-      assertThrows[LimitOffsetException] {
-        LimitOffsetQuery(limit = -1)
-      }
+      assertException(limit = 0)
+      assertException(limit = -1)
     }
   }
 
