bf584bf23c79fb860ca5e60ebfad999c3449941e
Andrey S <norwegianwood409@gmail.com>
2022-11-20 16:13:26 +0500

    [Red] Заглушка для параметров Setup

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/SetupTestSuite.scala
@@ -1,15 +1,17 @@
 package rr64.developer.infrastructure.dev.behavior
 
+import akka.actor.typed.scaladsl.TimerScheduler
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import rr64.developer.infrastructure.dev.behavior.Setup.FactorException
 
-class SetupTestSuite extends AnyWordSpec with Matchers {
+class SetupTestSuite extends AnyWordSpec with Matchers with MockFactory {
 
   private def setupFromWorkFactor(workFactor: Int) = Setup(
     workFactor = workFactor,
-    restFactor = ???,
-    timer = ???
+    restFactor = 10,
+    timer = mock[TimerScheduler[Command]]
   )
 
   /** Рабочий множитель */
