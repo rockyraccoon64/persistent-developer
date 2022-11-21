01d82cf7862dd89a4fb48ea74040e22ca4319780
Andrey S <norwegianwood409@gmail.com>
2022-11-21 13:24:11 +0500

    [Red] Исправление некорректного теста

--- src/test/scala/rr64/developer/infrastructure/task/query/LimitOffsetQueryStringExtractorTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/task/query/LimitOffsetQueryStringExtractorTestSuite.scala
@@ -1,11 +1,13 @@
 package rr64.developer.infrastructure.task.query
 
+import org.scalatest.EitherValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class LimitOffsetQueryStringExtractorTestSuite
   extends AnyWordSpec
-  with Matchers {
+    with Matchers
+    with EitherValues {
 
   trait ExtractorTest {
     private val factory = new LimitOffsetQueryFactory(defaultLimit = 10, maxLimit = 30)
@@ -19,18 +21,16 @@ class LimitOffsetQueryStringExtractorTestSuite
     /** Должен парсить корректно сформированный запрос */
     "extract correct queries" in new ExtractorTest {
       val input = Some("limit:10,offset:55")
-      val result = extractor.extract(input)
-      result.map { query =>
-        query.limit shouldEqual 10
-        query.offset shouldEqual 55
-      }
+      val result = extractor.extract(input).right.value
+      result.limit shouldEqual 10
+      result.offset shouldEqual 55
     }
 
     /** Должен возвращать сообщение об ошибке, когда запрос сформирован некорректно */
     "return an error message when the query format is incorrect" in new ExtractorTest {
       val input = Some("What's this query")
       val result = extractor.extract(input)
-      result shouldEqual Left(errorMessage)
+      result.left.value shouldEqual errorMessage
     }
 
   }
