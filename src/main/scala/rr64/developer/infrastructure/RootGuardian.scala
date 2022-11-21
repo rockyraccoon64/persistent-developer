7ac1b975a980f6b1b21bb4cad942baa97d652fae
Andrey S <norwegianwood409@gmail.com>
2022-11-21 18:59:48 +0500

    Супервизия актора разработчика

--- src/main/scala/rr64/developer/infrastructure/RootGuardian.scala
+++ src/main/scala/rr64/developer/infrastructure/RootGuardian.scala
@@ -1,8 +1,10 @@
 package rr64.developer.infrastructure
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import rr64.developer.infrastructure.dev.behavior.DeveloperBehavior.{DeveloperCommand, DeveloperRef}
+
+import scala.concurrent.duration.DurationInt
 
 object RootGuardian {
 
@@ -16,7 +18,14 @@ object RootGuardian {
   def apply(): Behavior[Message] = Behaviors.setup { ctx =>
     Behaviors.receiveMessage {
       case SpawnDeveloper(name, behavior, replyTo) =>
-        val ref = ctx.spawn(behavior, name) // TODO supervision
+        val strategy = SupervisorStrategy.restartWithBackoff(
+          minBackoff = 1.second,
+          maxBackoff = 30.seconds,
+          randomFactor = 0.2
+        )
+        val supervisedBehavior =
+          Behaviors.supervise(behavior).onFailure(strategy)
+        val ref = ctx.spawn(supervisedBehavior, name)
         replyTo ! ref
         Behaviors.empty
     }
