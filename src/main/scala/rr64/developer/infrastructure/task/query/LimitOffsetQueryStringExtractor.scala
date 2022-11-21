c5815e4bde0736b2c4da220285baee5751286349
Andrey S <norwegianwood409@gmail.com>
2022-11-21 13:08:18 +0500

    [Refactor] Перенос парсера в отдельный файл

---
+++ src/main/scala/rr64/developer/infrastructure/task/query/LimitOffsetQueryStringExtractor.scala
@@ -0,0 +1,32 @@
+package rr64.developer.infrastructure.task.query
+
+import rr64.developer.infrastructure.api.QueryExtractor
+import rr64.developer.infrastructure.task.query.LimitOffsetQueryStringExtractor._
+
+import scala.util.Try
+import scala.util.matching.Regex
+
+class LimitOffsetQueryStringExtractor(factory: LimitOffsetQueryFactory)
+  extends QueryExtractor[Option[String], LimitOffsetQuery] {
+
+  override def extract(input: Option[String]): Either[String, LimitOffsetQuery] =
+    input match {
+      case Some(regex(limitStr, offsetStr)) =>
+        Try {
+          factory.create(
+            limit = limitStr.toInt,
+            offset = offsetStr.toInt
+          )
+        }.toOption.toRight(errorMessage)
+      case Some(_) =>
+        Left(errorMessage)
+      case None =>
+        Right(factory.Default)
+    }
+
+}
+
+object LimitOffsetQueryStringExtractor {
+  private val regex: Regex = """^limit:(\d),offset:(\d+)$""".r
+  private val errorMessage = "Invalid limit + offset"
+}
