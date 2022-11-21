ebb85e72f8580f4e8e7eb1f88adf3b48d6c2d212
Andrey S <norwegianwood409@gmail.com>
2022-11-21 12:56:34 +0500

    [Green] Парсер запроса должен парсить корректно сформированный запрос

--- src/main/scala/rr64/developer/infrastructure/task/LimitOffsetQuery.scala
+++ src/main/scala/rr64/developer/infrastructure/task/LimitOffsetQuery.scala
@@ -1,8 +1,35 @@
 package rr64.developer.infrastructure.task
 
+import rr64.developer.infrastructure.api.QueryExtractor
+
+import scala.util.Try
+import scala.util.matching.Regex
+
 trait LimitOffsetQuery {
   def limit: Int
   def offset: Int
+}
+
+object LimitOffsetQuery {
+
+  private val regex: Regex = """^limit:(\d),offset:(\d+)$""".r
+
+  private val errorMessage = "Invalid limit + offset"
+
+  def stringExtractor(factory: LimitOffsetQueryFactory): QueryExtractor[Option[String], LimitOffsetQuery] = {
+    case Some(regex(limitStr, offsetStr)) =>
+      Try {
+        factory.create(
+          limit = limitStr.toInt,
+          offset = offsetStr.toInt
+        )
+      }.toOption.toRight(errorMessage)
+    case Some(_) =>
+      Left(errorMessage)
+    case None =>
+      Right(factory.Default)
+  }
+
 }
 
 class LimitOffsetException extends RuntimeException
