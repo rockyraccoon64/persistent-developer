50286cf74a4a055e81d9ea8d2d99498407bf7e58
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:11:23 +0500

    Комментарий к адаптеру

--- src/main/scala/rr64/developer/infrastructure/api/Adapter.scala
+++ src/main/scala/rr64/developer/infrastructure/api/Adapter.scala
@@ -1,5 +1,11 @@
 package rr64.developer.infrastructure.api
 
+/**
+ * Адаптер одного типа к другому
+ * */
 trait Adapter[From, To] {
+
+  /** Преобразовать значение */
   def convert(value: From): To
+
 }
