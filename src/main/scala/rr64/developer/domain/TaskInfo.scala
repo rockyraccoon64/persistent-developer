3fd31525e8d70c61b2ae00d95f050502b13a8571
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:07:29 +0500

    Комментарий к существующим задачам

--- src/main/scala/rr64/developer/domain/TaskInfo.scala
+++ src/main/scala/rr64/developer/domain/TaskInfo.scala
@@ -2,4 +2,10 @@ package rr64.developer.domain
 
 import java.util.UUID
 
+/**
+ * Информация о существующей задаче
+ * @param id Идентификатор
+ * @param difficulty Сложность
+ * @param status Текущий статус
+ * */
 case class TaskInfo(id: UUID, difficulty: Difficulty, status: TaskStatus)
