43cbb09a5243ab04a20347d52bf822bcb03d5255
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:19:32 +0500

    Комментарии к общим JSON-форматам

--- src/main/scala/rr64/developer/infrastructure/api/CommonJsonFormats.scala
+++ src/main/scala/rr64/developer/infrastructure/api/CommonJsonFormats.scala
@@ -4,7 +4,12 @@ import spray.json.{JsString, JsValue, JsonFormat, deserializationError}
 
 import java.util.UUID
 
+/**
+ * Общие JSON-форматы
+ * */
 trait CommonJsonFormats {
+
+  /** JSON-формат UUID */
   implicit val uuidJsonFormat: JsonFormat[UUID] = new JsonFormat[UUID] {
     override def write(obj: UUID): JsValue = JsString(obj.toString)
     override def read(json: JsValue): UUID = json match {
@@ -12,6 +17,7 @@ trait CommonJsonFormats {
       case _ => deserializationError("Invalid UUID format")
     }
   }
+
 }
 
 object CommonJsonFormats extends CommonJsonFormats
