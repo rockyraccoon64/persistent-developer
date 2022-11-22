ac687436557e78840fd2ec61ea1fdeedf4bdf2e0
Andrey S <norwegianwood409@gmail.com>
2022-11-22 15:59:08 +0500

    Комментарии к интерфейсу сервиса разработки задач

--- src/main/scala/rr64/developer/domain/DeveloperService.scala
+++ src/main/scala/rr64/developer/domain/DeveloperService.scala
@@ -3,9 +3,31 @@ package rr64.developer.domain
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Сервис разработки задач
+ * @tparam Query Параметры запроса списка задач
+ * */
 trait DeveloperService[Query] {
+
+  /**
+   * Поручить разработчику задачу
+   * @param task Новая задача
+   * */
   def addTask(task: Task)(implicit ec: ExecutionContext): Future[DeveloperReply]
+
+  /** Текущее состояние разработчика */
   def developerState(implicit ec: ExecutionContext): Future[DeveloperState]
+
+  /**
+   * Информация о задаче
+   * @param id Идентификатор задачи
+   * */
   def taskInfo(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]]
-  def tasks(q: Query)(implicit ec: ExecutionContext): Future[Seq[TaskInfo]]
+
+  /**
+   * Запросить список задач
+   * @param query Параметры запроса
+   * */
+  def tasks(query: Query)(implicit ec: ExecutionContext): Future[Seq[TaskInfo]]
+
 }
