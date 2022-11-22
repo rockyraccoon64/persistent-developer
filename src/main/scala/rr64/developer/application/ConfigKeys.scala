dbcaecae626e3af4b9a7e7b31aa97db10d81e85a
Andrey S <norwegianwood409@gmail.com>
2022-11-22 15:48:38 +0500

    Комментарии к ключам файла конфигурации

--- src/main/scala/rr64/developer/application/ConfigKeys.scala
+++ src/main/scala/rr64/developer/application/ConfigKeys.scala
@@ -1,15 +1,41 @@
 package rr64.developer.application
 
+/**
+ * Ключи файла конфигурации
+ */
 object ConfigKeys {
+
+  /** Конфигурация приложения */
   val AppConfig = "persistent-dev"
+
+  /** Максимальное время ожидания запросов акторам */
   val AskTimeout = "ask-timeout"
+
+  /** Имя корневого актора */
   val RootGuardianName = "root-guardian-name"
+
+  /** Имя актора разработчика */
   val DeveloperActorName = "developer.name"
+
+  /** Persistence ID актора разработчика */
   val DeveloperPersistenceId = "developer.persistence-id"
+
+  /** Рабочий множитель */
   val WorkFactor = "developer.work-factor"
+
+  /** Множитель отдыха */
   val RestFactor = "developer.rest-factor"
+
+  /** Длина списка задач в запросе по умолчанию */
   val DefaultLimit = "tasks.default-limit"
+
+  /** Максимальная длина списка задач в запросе */
   val MaxLimit = "tasks.max-limit"
+
+  /** Интерфейс REST API */
   val ApiInterface = "api.interface"
+
+  /** Порт REST API */
   val ApiPort = "api.port"
+
 }
