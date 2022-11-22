f515722c12ae0db5ba7eae06a7171bf5f55dfc84
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:08:52 +0500

    Комментарии к сервису поиска задач

--- src/main/scala/rr64/developer/domain/Tasks.scala
+++ src/main/scala/rr64/developer/domain/Tasks.scala
@@ -3,7 +3,22 @@ package rr64.developer.domain
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Сервис поиска задач
+ * @tparam Query Параметры запроса списка задач
+ */
 trait Tasks[Query] {
+
+  /**
+   * Найти задачу по идентификатору
+   * @param id Идентификатор задачи
+   * */
   def findById(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]]
+
+  /**
+   * Запросить список задач
+   * @param query Параметры запроса
+   * */
   def list(query: Query)(implicit ec: ExecutionContext): Future[Seq[TaskInfo]]
+
 }
