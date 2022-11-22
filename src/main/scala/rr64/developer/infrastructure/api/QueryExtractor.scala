8dc8e4b80fe34367cc2d69f45094ac23541a4af6
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:20:46 +0500

    Комментарии к парсеру запроса

--- src/main/scala/rr64/developer/infrastructure/api/QueryExtractor.scala
+++ src/main/scala/rr64/developer/infrastructure/api/QueryExtractor.scala
@@ -1,5 +1,17 @@
 package rr64.developer.infrastructure.api
 
+/**
+ * Парсер запроса
+ * @tparam Input Входные данные
+ * @tparam Query Запрос
+ * */
 trait QueryExtractor[Input, Query] {
+
+  /**
+   * Извлечь запрос
+   * @param input Входные данные
+   * @return Извлечённый запрос или сообщение об ошибке
+   * */
   def extract(input: Input): Either[String, Query]
+
 }
