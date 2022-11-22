a1c9d727d312cd9c179983dcfc1484caf0ec4e8a
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:12:37 +0500

    Комментарии к состоянию разработчика для REST API

--- src/main/scala/rr64/developer/infrastructure/api/ApiDeveloperState.scala
+++ src/main/scala/rr64/developer/infrastructure/api/ApiDeveloperState.scala
@@ -4,17 +4,25 @@ import rr64.developer.domain.DeveloperState
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
+/**
+ * Состояние разработчика для REST API
+ * */
 case class ApiDeveloperState private(state: String)
 
 object ApiDeveloperState {
 
+  /** Свободен */
   val Free: ApiDeveloperState = ApiDeveloperState("Free")
+  /** Работает */
   val Working: ApiDeveloperState = ApiDeveloperState("Working")
+  /** Отдыхает */
   val Resting: ApiDeveloperState = ApiDeveloperState("Resting")
 
+  /** JSON-формат состояния разработчика */
   implicit val developerStateJsonFormat: RootJsonFormat[ApiDeveloperState] =
     jsonFormat1(ApiDeveloperState.apply)
 
+  /** Адаптер доменного состояния разработчика к используемому в REST API */
   implicit val adapter: Adapter[DeveloperState, ApiDeveloperState] = {
     case DeveloperState.Free => Free
     case DeveloperState.Working => Working
