e5d1b21767524d3121da4541777277f5042864f1
Andrey S <norwegianwood409@gmail.com>
2022-11-22 20:06:01 +0500

    Комментарии к задаче с идентификатором

--- src/main/scala/rr64/developer/infrastructure/task/TaskWithId.scala
+++ src/main/scala/rr64/developer/infrastructure/task/TaskWithId.scala
@@ -4,15 +4,32 @@ import rr64.developer.domain.{Difficulty, Task}
 
 import java.util.UUID
 
+/**
+ * Задача с присвоенным ей идентификатором
+ * @param task Задача
+ * @param id Идентификатор задачи
+ * */
 case class TaskWithId(task: Task, id: UUID) {
+
+  /** Сложность задачи */
   def difficulty: Difficulty = task.difficulty
+
 }
 
 object TaskWithId {
 
+  /**
+   * Присвоить задаче идентификатор
+   * @param task Задача
+   * */
   def fromTask(task: Task): TaskWithId =
     TaskWithId(task, UUID.randomUUID())
 
+  /**
+   * Задача с присвоенным ей идентификатором
+   * @param difficulty Сложность задачи
+   * @param id Идентификатор задачи
+   * */
   def apply(difficulty: Int, id: UUID): TaskWithId =
     TaskWithId(Task(difficulty), id)
 
