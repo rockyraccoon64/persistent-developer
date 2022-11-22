f3f8140cf211b29d7207fc72a655dafbabb5aab3
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:18:41 +0500

    Комментарии к новой задаче для REST API

--- src/main/scala/rr64/developer/infrastructure/api/ApiTaskToAdd.scala
+++ src/main/scala/rr64/developer/infrastructure/api/ApiTaskToAdd.scala
@@ -3,9 +3,14 @@ package rr64.developer.infrastructure.api
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
+/**
+ * Представление новой задачи для REST API
+ * @param difficulty Сложность задачи
+ * */
 case class ApiTaskToAdd(difficulty: Int)
 
 object ApiTaskToAdd {
+  /** JSON-формат новой задачи */
   implicit val apiAddTaskJsonFormat: RootJsonFormat[ApiTaskToAdd] =
     jsonFormat1(ApiTaskToAdd.apply)
 }
