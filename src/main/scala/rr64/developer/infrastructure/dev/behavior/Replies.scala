5c619d080727d54771269f849d57c37759b4c54b
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:35:28 +0500

    Комментарии к параметрам ответов

--- src/main/scala/rr64/developer/infrastructure/dev/behavior/Replies.scala
+++ src/main/scala/rr64/developer/infrastructure/dev/behavior/Replies.scala
@@ -6,10 +6,20 @@ import java.util.UUID
  * Ответы актора разработчика
  * */
 object Replies {
+
   /** Результат добавления задачи */
   sealed trait AddTaskResult
-  /** Задача принята в работу */
+
+  /**
+   * Задача принята в работу
+   * @param id Идентификатор задачи
+   * */
   case class TaskStarted(id: UUID) extends AddTaskResult
-  /** Задача поставлена в очередь */
+
+  /**
+   * Задача поставлена в очередь
+   * @param id Идентификатор задачи
+   * */
   case class TaskQueued(id: UUID) extends AddTaskResult
+
 }
