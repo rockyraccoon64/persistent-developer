a780689925c3ab811f3373641e3d5633d1790e09
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:24:31 +0500

    Комментарии к REST API

--- src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
+++ src/main/scala/rr64/developer/infrastructure/api/RestApi.scala
@@ -13,33 +13,23 @@ import scala.util.{Failure, Success, Try}
 
 /**
  * REST API сервиса разработки
+ * @tparam Query Параметры запроса списка задач
+ * @param service Сервис разработки задач
+ * @param queryExtractor Парсер запроса списка задач
  */
 class RestApi[Query](
   service: DeveloperService[Query],
   queryExtractor: QueryExtractor[Option[String], Query]
 ) {
 
+  /**
+   * Адаптеры DTO
+   * */
   private val developerStateAdapter = implicitly[Adapter[DeveloperState, ApiDeveloperState]]
   private val taskInfoAdapter = implicitly[Adapter[TaskInfo, ApiTaskInfo]]
   private val replyAdapter = implicitly[Adapter[DeveloperReply, ApiReply]]
 
-  private val developerStateRoute =
-    (path("developer-state") & extractExecutionContext) { implicit exec =>
-      onSuccess(service.developerState) { value =>
-        complete(developerStateAdapter.convert(value))
-      }
-    }
-
-  private val taskInfoRoute =
-    path("task-info" / JavaUUID) { id =>
-      extractExecutionContext { implicit exec =>
-        onSuccess(service.taskInfo(id)) {
-          case Some(value) => complete(taskInfoAdapter.convert(value))
-          case None => complete(StatusCodes.NotFound)
-        }
-      }
-    }
-
+  /** API для команды поручения задачи */
   private val addTaskRoute =
     (path("add-task") & entity(as[ApiTaskToAdd])) { taskToAdd =>
       extractExecutionContext { implicit exec =>
@@ -54,6 +44,26 @@ class RestApi[Query](
       }
     }
 
+  /** API для запроса состояния разработчика */
+  private val developerStateRoute =
+    (path("developer-state") & extractExecutionContext) { implicit exec =>
+      onSuccess(service.developerState) { value =>
+        complete(developerStateAdapter.convert(value))
+      }
+    }
+
+  /** API для запроса информации о задаче */
+  private val taskInfoRoute =
+    path("task-info" / JavaUUID) { id =>
+      extractExecutionContext { implicit exec =>
+        onSuccess(service.taskInfo(id)) {
+          case Some(value) => complete(taskInfoAdapter.convert(value))
+          case None => complete(StatusCodes.NotFound)
+        }
+      }
+    }
+
+  /** API для запроса списка задач */
   private val taskListRoute =
     (path("task-list") & parameter("query".as[String].optional)) { query =>
       extractExecutionContext { implicit exec =>
@@ -68,6 +78,7 @@ class RestApi[Query](
       }
     }
 
+  /** Полный API */
   val route: Route = Route.seal(
     pathPrefix("api") {
       (pathPrefix("query") & get) {
