7f7a3fffb7230c13b70e6dbc052e03da68aa055c
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:02:42 +0500

    Комментарии к состоянию разработчика

--- src/main/scala/rr64/developer/domain/DeveloperState.scala
+++ src/main/scala/rr64/developer/domain/DeveloperState.scala
@@ -1,11 +1,20 @@
 package rr64.developer.domain
 
+/**
+ * Состояние разработчика
+ * */
 sealed trait DeveloperState
 
 object DeveloperState {
+
+  /** Свободен */
   case object Free extends DeveloperState
+  /** Работает */
   case object Working extends DeveloperState
+  /** Отдыхает */
   case object Resting extends DeveloperState
 
+  /** Начальное состояние */
   val InitialState: DeveloperState = Free
+
 }
