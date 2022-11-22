9a872797704c8bc8f89588ff715e2241ebc2d593
Andrey S <norwegianwood409@gmail.com>
2022-11-22 20:06:43 +0500

    Комментарии к кодеку

--- src/main/scala/rr64/developer/infrastructure/Codec.scala
+++ src/main/scala/rr64/developer/infrastructure/Codec.scala
@@ -1,6 +1,14 @@
 package rr64.developer.infrastructure
 
+/**
+ * Кодек (двустороннее преобразование)
+ * */
 trait Codec[A, B] {
+
+  /** Кодировать значение */
   def encode(value: A): B
+
+  /** Декодировать значение */
   def decode(value: B): A
+
 }
