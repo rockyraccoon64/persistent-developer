e7ef73603257f9f1c1358b176afa976e05363c4d
Andrey S <norwegianwood409@gmail.com>
2022-11-22 19:07:05 +0500

    Комментарии к репозиторию задач

--- src/main/scala/rr64/developer/infrastructure/task/TaskRepository.scala
+++ src/main/scala/rr64/developer/infrastructure/task/TaskRepository.scala
@@ -5,8 +5,27 @@ import rr64.developer.domain.TaskInfo
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Репозиторий задач
+ * @tparam Query Параметры запроса списка задач
+ * */
 trait TaskRepository[Query] {
+
+  /**
+   * Сохранить задачу
+   * @param taskInfo Задача
+   * */
   def save(taskInfo: TaskInfo): Future[_]
+
+  /**
+   * Найти задачу по идентификатору
+   * @param id Идентификатор задачи
+   * */
   def findById(id: UUID)(implicit ec: ExecutionContext): Future[Option[TaskInfo]]
-  def list(q: Query)(implicit ec: ExecutionContext): Future[Seq[TaskInfo]]
+
+  /**
+   * Запрос списка задач
+   * @param query Параметры запроса
+   * */
+  def list(query: Query)(implicit ec: ExecutionContext): Future[Seq[TaskInfo]]
 }
