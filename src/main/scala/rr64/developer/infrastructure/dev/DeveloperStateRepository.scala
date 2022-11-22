cebf4b5b8f784c3515434b00d3428a48df7ad517
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:49:11 +0500

    Комментарий к репозиторию состояний разработчиков

--- src/main/scala/rr64/developer/infrastructure/dev/DeveloperStateRepository.scala
+++ src/main/scala/rr64/developer/infrastructure/dev/DeveloperStateRepository.scala
@@ -4,7 +4,22 @@ import rr64.developer.domain.DeveloperState
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Репозиторий состояний разработчиков
+ * */
 trait DeveloperStateRepository {
+
+  /**
+   * Сохранить состояние
+   * @param id Идентификатор разработчика
+   * @param state Состояние разработчика
+   * */
   def save(id: String, state: DeveloperState)(implicit ec: ExecutionContext): Future[_]
+
+  /**
+   * Поиск состояния разработчика по идентификатору
+   * @param id Идентификатор разработчика
+   * */
   def findById(id: String)(implicit ec: ExecutionContext): Future[Option[DeveloperState]]
+
 }
