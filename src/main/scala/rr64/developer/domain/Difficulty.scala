94a76c059240165ad5343f81833fd5f07772fdf0
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:04:23 +0500

    Комментарии к сложности задачи

--- src/main/scala/rr64/developer/domain/Difficulty.scala
+++ src/main/scala/rr64/developer/domain/Difficulty.scala
@@ -1,16 +1,20 @@
 package rr64.developer.domain
 
+/**
+ * Сложность задачи
+ * @param value Значение в рамках 1-100
+ */
 case class Difficulty private(value: Int)
 
 object Difficulty {
 
-  def apply(value: Int): Difficulty = {
+  def apply(value: Int): Difficulty =
     if (value > 0 && value <= 100)
       new Difficulty(value)
     else
       throw new DifficultyException
-  }
 
+  /** Исключение, связанное с некорректной сложностью задачи */
   class DifficultyException
     extends RuntimeException("Difficulty should be in the range [1-100]")
 
