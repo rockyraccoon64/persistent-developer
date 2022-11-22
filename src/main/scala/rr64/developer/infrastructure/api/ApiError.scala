6755359411740d16f6663ff9dd8e571185e6346f
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:13:39 +0500

    Комментарии к ошибкам для REST API

--- src/main/scala/rr64/developer/infrastructure/api/ApiError.scala
+++ src/main/scala/rr64/developer/infrastructure/api/ApiError.scala
@@ -3,15 +3,23 @@ package rr64.developer.infrastructure.api
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
+/**
+ * Представление ошибки для REST API
+ * */
 case class ApiError(error: String, message: String)
 
 object ApiError {
-  implicit val apiErrorJsonFormat: RootJsonFormat[ApiError] =
-    jsonFormat2(ApiError.apply)
 
+  /** Ошибка в запросе */
   def inQuery(message: String): ApiError =
     ApiError("Query", message)
 
+  /** Ошибка, связанная со сложностью задачи */
   val TaskDifficulty: ApiError =
     ApiError("DifficultyRange", "Difficulty should be in the range [1-100]")
+
+  /** JSON-формат ошибки */
+  implicit val apiErrorJsonFormat: RootJsonFormat[ApiError] =
+    jsonFormat2(ApiError.apply)
+
 }
