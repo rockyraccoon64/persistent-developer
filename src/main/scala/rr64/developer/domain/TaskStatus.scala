b97409a1cb2ef98a8ae9e4315f385db5e9b2b848
Andrey S <norwegianwood409@gmail.com>
2022-11-22 16:09:32 +0500

    Комментарии к статусу задачи

--- src/main/scala/rr64/developer/domain/TaskStatus.scala
+++ src/main/scala/rr64/developer/domain/TaskStatus.scala
@@ -1,9 +1,15 @@
 package rr64.developer.domain
 
+/**
+ * Статус задачи
+ * */
 sealed trait TaskStatus
 
 object TaskStatus {
+  /** В работе */
   case object InProgress extends TaskStatus
+  /** В очереди */
   case object Queued extends TaskStatus
+  /** Завершена */
   case object Finished extends TaskStatus
 }
