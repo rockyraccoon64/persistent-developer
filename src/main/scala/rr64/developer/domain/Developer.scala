a73462fca23bd89dd2e62254403e548c38f6b793
Andrey S <norwegianwood409@gmail.com>
2022-11-22 15:54:20 +0500

    Комментарии к разработчику

--- src/main/scala/rr64/developer/domain/Developer.scala
+++ src/main/scala/rr64/developer/domain/Developer.scala
@@ -2,7 +2,15 @@ package rr64.developer.domain
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Разработчик
+ * */
 trait Developer {
+
+  /** Текущее состояние */
   def state(implicit ec: ExecutionContext): Future[DeveloperState]
+  
+  /** Поручить задачу */
   def addTask(task: Task)(implicit ec: ExecutionContext): Future[DeveloperReply]
+
 }
