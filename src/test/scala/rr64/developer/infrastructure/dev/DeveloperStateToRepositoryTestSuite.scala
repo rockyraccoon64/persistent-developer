ca2b5f2a85fbded252fe6609a9daf769bf6af3e9
Andrey S <norwegianwood409@gmail.com>
2022-11-23 12:29:00 +0500

    Перевод набора тестов обработчика проекции состояния разработчика на WordSpec

--- src/test/scala/rr64/developer/infrastructure/dev/DeveloperStateToRepositoryTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/DeveloperStateToRepositoryTestSuite.scala
@@ -10,6 +10,7 @@ import akka.projection.testkit.scaladsl.{ProjectionTestKit, TestProjection}
 import akka.stream.scaladsl.Source
 import org.scalatest.Assertion
 import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.wordspec.AnyWordSpecLike
 import rr64.developer.domain.dev.DeveloperState
 import rr64.developer.domain.task.Task
 import rr64.developer.infrastructure.ProjectionTestUtils
@@ -23,7 +24,7 @@ import scala.concurrent.{ExecutionContext, Future}
  * */
 class DeveloperStateToRepositoryTestSuite
   extends ScalaTestWithActorTestKit
-  with AnyFlatSpecLike {
+    with AnyWordSpecLike {
 
   private val projectionTestKit = ProjectionTestKit(system)
   private implicit val ec: ExecutionContext = system.executionContext
@@ -73,93 +74,96 @@ class DeveloperStateToRepositoryTestSuite
   private val defaultTask1 = Task(1).withRandomId
   private val defaultTask2 = Task(5).withRandomId
 
-  /** Обработчик проекции должен обновлять состояние разработчика на "Работает", когда он начинает задачу */
-  "The handler" should "update the developer state in the repository when a task is started" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) :: Nil
-    val projection = projectionFromEvents(events)
+  /** Обработчик проекции */
+  "The handler" should {
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Working)
+    /** Должен обновлять состояние разработчика на "Работает", когда он начинает задачу */
+    "update the developer state in the repository when a task is started" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) :: Nil
+      val projection = projectionFromEvents(events)
+
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Working)
+      }
     }
-  }
 
-  /** Обработчик проекции должен обновлять состояние разработчика на "Отдых", когда он заканчивает задачу */
-  "The handler" should "update the developer state in the repository when a task is finished" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) ::
-      Event.TaskFinished(defaultTask1) :: Nil
-    val projection = projectionFromEvents(events)
+    /** Должен обновлять состояние разработчика на "Отдых", когда он заканчивает задачу */
+    "update the developer state in the repository when a task is finished" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) ::
+        Event.TaskFinished(defaultTask1) :: Nil
+      val projection = projectionFromEvents(events)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Resting)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Resting)
+      }
     }
-  }
 
-  /** Обработчик проекции должен обновлять состояние разработчика на "Свободен",
-   * когда он отдохнул и у него нет задач */
-  "The handler" should "update the state to Free after the developer rests if he has no more tasks" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) ::
-      Event.TaskFinished(defaultTask1) ::
-      Event.Rested(None) ::
-      Nil
-    val projection = projectionFromEvents(events)
+    /** Должен обновлять состояние разработчика на "Свободен", когда он отдохнул и у него нет задач */
+    "update the state to Free after the developer rests if he has no more tasks" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) ::
+        Event.TaskFinished(defaultTask1) ::
+        Event.Rested(None) ::
+        Nil
+      val projection = projectionFromEvents(events)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Free)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Free)
+      }
     }
-  }
 
-  /** Обработчик проекции должен обновлять состояние разработчика на "Работает",
-   * когда он отдохнул и в очереди есть задача */
-  "The handler" should "update the state to Working after the developer rests if there is a task in the queue" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) ::
-      Event.TaskQueued(defaultTask2) ::
-      Event.TaskFinished(defaultTask1) ::
-      Event.Rested(Some(defaultTask2)) ::
-      Nil
-    val projection = projectionFromEvents(events)
+    /** Должен обновлять состояние разработчика на "Работает", когда он отдохнул и в очереди есть задача */
+    "update the state to Working after the developer rests if there is a task in the queue" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) ::
+        Event.TaskQueued(defaultTask2) ::
+        Event.TaskFinished(defaultTask1) ::
+        Event.Rested(Some(defaultTask2)) ::
+        Nil
+      val projection = projectionFromEvents(events)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Working)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Working)
+      }
     }
-  }
 
-  /** Обработчик проекции не должен обновлять состояние разработчика при получении задачи, когда он работает */
-  "The handler" should "not update the state after receiving a new task while working" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) ::
-      Event.TaskQueued(defaultTask2) ::
-      Nil
-    val projection = projectionFromEvents(events)
+    /** Не должен обновлять состояние разработчика при получении задачи, когда он работает */
+    "not update the state after receiving a new task while working" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) ::
+        Event.TaskQueued(defaultTask2) ::
+        Nil
+      val projection = projectionFromEvents(events)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Working)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Working)
+      }
     }
-  }
 
-  /** Обработчик проекции не должен обновлять состояние разработчика при получении задачи, когда он отдыхает */
-  "The handler" should "not update the state when the developer receives a new task while resting" in new Fixture {
-    val events = Event.TaskStarted(defaultTask1) ::
-      Event.TaskFinished(defaultTask1) ::
-      Event.TaskQueued(defaultTask2) ::
-      Nil
-    val projection = projectionFromEvents(events)
+    /** Не должен обновлять состояние разработчика при получении задачи, когда он отдыхает */
+    "not update the state when the developer receives a new task while resting" in new Fixture {
+      val events = Event.TaskStarted(defaultTask1) ::
+        Event.TaskFinished(defaultTask1) ::
+        Event.TaskQueued(defaultTask2) ::
+        Nil
+      val projection = projectionFromEvents(events)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Resting)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Resting)
+      }
     }
-  }
 
-  /** Для каждого разработчика состояние обновляется отдельно */
-  "The handler" should "update states for different developers separately" in new Fixture {
-    val differentPersistenceId = "test-id2"
-    val events1 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask2) :: Event.TaskFinished(defaultTask2) :: Nil, defaultPersistenceId)
-    val events2 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask1) :: Nil, differentPersistenceId, startOffset = 2)
+    /** Для каждого разработчика состояние обновляется отдельно */
+    "update states for different developers separately" in new Fixture {
+      val differentPersistenceId = "test-id2"
+      val events1 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask2) :: Event.TaskFinished(defaultTask2) :: Nil, defaultPersistenceId)
+      val events2 = ProjectionTestUtils.envelopeSource[Event](Event.TaskStarted(defaultTask1) :: Nil, differentPersistenceId, startOffset = 2)
 
-    val projection = projectionFromSource(events1 concat events2)
+      val projection = projectionFromSource(events1 concat events2)
 
-    projectionTestKit.run(projection) {
-      assertState(DeveloperState.Resting, defaultPersistenceId)
-      assertState(DeveloperState.Working, differentPersistenceId)
+      projectionTestKit.run(projection) {
+        assertState(DeveloperState.Resting, defaultPersistenceId)
+        assertState(DeveloperState.Working, differentPersistenceId)
+      }
     }
+
   }
 
 }
