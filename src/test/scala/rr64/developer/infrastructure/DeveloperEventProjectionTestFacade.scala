d0e49c09076a83ff6c2d439d76bee5d1fe70ef86
Andrey S <norwegianwood409@gmail.com>
2022-12-09 21:25:41 +0500

    Комментарии к тестовым фасадам

--- src/test/scala/rr64/developer/infrastructure/DeveloperEventProjectionTestFacade.scala
+++ src/test/scala/rr64/developer/infrastructure/DeveloperEventProjectionTestFacade.scala
@@ -5,11 +5,17 @@ import akka.projection.eventsourced.EventEnvelope
 import akka.projection.scaladsl.Handler
 import akka.projection.testkit.scaladsl.TestProjection
 
+/**
+ * Тестовый фасад для проекций событий разработчика
+ * */
 trait DeveloperEventProjectionTestFacade
   extends EventProjectionTestFacade
     with DeveloperEventTestFacade {
 
+  /** Обработчик проекции */
   type ProjHandler = Handler[EventEnvelope[Event]]
+
+  /** Тестовая проекция */
   type TestProj = TestProjection[Offset, EventEnvelope[Event]]
 
 }
--- src/test/scala/rr64/developer/infrastructure/DeveloperEventTestFacade.scala
+++ src/test/scala/rr64/developer/infrastructure/DeveloperEventTestFacade.scala
@@ -3,19 +3,27 @@ package rr64.developer.infrastructure
 import rr64.developer.infrastructure.dev.behavior.Event
 import rr64.developer.infrastructure.task.TaskWithId
 
+/**
+ * Фасад для тестов с использованием событий актора разработчика
+ * */
 trait DeveloperEventTestFacade {
 
+  /** Событие */
   type Event = rr64.developer.infrastructure.dev.behavior.Event
 
+  /** Задача начата */
   def taskStartedEvent(taskWithId: TaskWithId): Event.TaskStarted =
     Event.TaskStarted(taskWithId)
 
+  /** Задача поставлена в очередь */
   def taskQueuedEvent(taskWithId: TaskWithId): Event.TaskQueued =
     Event.TaskQueued(taskWithId)
 
+  /** Задача завершена */
   def taskFinishedEvent(taskWithId: TaskWithId): Event.TaskFinished =
     Event.TaskFinished(taskWithId)
 
+  /** Отдых завершён */
   def restedEvent(nextTask: Option[TaskWithId]): Event.Rested =
     Event.Rested(nextTask)
 
