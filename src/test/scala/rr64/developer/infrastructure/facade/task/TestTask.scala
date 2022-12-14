2f20ba7bdc62b7851150205d8b168eca5f1092a1
Andrey S <norwegianwood409@gmail.com>
2022-12-14 22:16:40 +0500

    Комментарии для тестовых заместителей задач

--- src/test/scala/rr64/developer/infrastructure/facade/task/TestTask.scala
+++ src/test/scala/rr64/developer/infrastructure/facade/task/TestTask.scala
@@ -2,6 +2,12 @@ package rr64.developer.infrastructure.facade.task
 
 import rr64.developer.domain.task.Task
 
+/**
+ * Тестовый заместитель для задач
+ * */
 case class TestTask(difficulty: Int) {
+
+  /** Преобразовать в доменную задачу */
   private[facade] def toDomain: Task = Task(difficulty)
+
 }
--- src/test/scala/rr64/developer/infrastructure/facade/task/TestTaskIdentifier.scala
+++ src/test/scala/rr64/developer/infrastructure/facade/task/TestTaskIdentifier.scala
@@ -2,4 +2,7 @@ package rr64.developer.infrastructure.facade.task
 
 import java.util.UUID
 
+/**
+ * Тестовый заместитель для идентификаторов задач
+ * */
 class TestTaskIdentifier private[facade](private[facade] val id: UUID)
