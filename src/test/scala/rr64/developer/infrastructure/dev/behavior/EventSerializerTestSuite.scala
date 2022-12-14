7a4beffeb691176c54a2fdc3bf0b7987847df688
Andrey S <norwegianwood409@gmail.com>
2022-12-14 18:04:29 +0500

    Использование фасада для событий в тестах сериализатора событий

--- src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
+++ src/test/scala/rr64/developer/infrastructure/dev/behavior/EventSerializerTestSuite.scala
@@ -2,6 +2,7 @@ package rr64.developer.infrastructure.dev.behavior
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import rr64.developer.infrastructure.DeveloperEventTestFacade._
 import rr64.developer.infrastructure.task.TaskWithId
 
 /**
@@ -21,34 +22,34 @@ class EventSerializerTestSuite extends AnyFlatSpec with Matchers {
   /** Сериализация события "Задача поставлена в очередь" */
   "The Task Queued event" should "be serialized" in {
     val task = TaskWithId(3, "3b93d086-16f1-410e-9325-4c2e220bef8f")
-    val event = Event.TaskQueued(task)
+    val event = taskQueuedEvent(task)
     assertSerialized(event)
   }
 
   /** Сериализация события "Задача передана в разработку" */
   "The Task Started event" should "be serialized" in {
     val task = TaskWithId(19, "39ea0ce2-e56f-42a8-aa4c-0a3cd496894e")
-    val event = Event.TaskStarted(task)
+    val event = taskStartedEvent(task)
     assertSerialized(event)
   }
 
   /** Сериализация события "Задача завершена" */
   "The Task Finished event" should "be serialized" in {
     val task = TaskWithId(97, "94b9c4e5-03f8-4f2c-aed9-1f1a22fd1e3b")
-    val event = Event.TaskFinished(task)
+    val event = taskFinishedEvent(task)
     assertSerialized(event)
   }
 
   /** Сериализация события "Отдых завершён" с задачей в очереди */
   "The Rested event" should "be serialized when there's a next task" in {
     val task = TaskWithId(55, "54ef15ab-8eb5-4a96-ad52-cd608b0f86f7")
-    val event = Event.Rested(Some(task))
+    val event = restedEvent(Some(task))
     assertSerialized(event)
   }
 
   /** Сериализация события "Отдых завершён" без задач в очереди */
   "The Rested event" should "be serialized when there is no next task" in {
-    val event = Event.Rested(None)
+    val event = restedEvent(None)
     assertSerialized(event)
   }
 
